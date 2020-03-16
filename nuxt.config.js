e19e0ab05871247984ee81cca46a84d11723f580
Houssem Nedjar <h.nedjar@univerweb.dz>
2020-03-16 13:17:26 +0100

    Ajout favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,14 +7,18 @@ export default {
     title: process.env.npm_package_name || '',
     meta: [
       { charset: 'utf-8' },
+      { name: 'description', hid: 'description', content: process.env.npm_package_description || '' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      {
-        hid: 'description',
-        name: 'description',
-        content: process.env.npm_package_description || ''
-      }
+      { name: 'msapplication-TileColor', content: '#50c8f0' },
+      { name: 'theme-color', content: '#50c8f0' }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [
+      { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+      { rel: 'manifest', href: '/site.webmanifest' },
+      { rel: 'mask-icon', type: '/safari-pinned-tab.svg', color: '#50c8f0' }
+    ]
   },
   /*
    ** Customize the progress-bar color
