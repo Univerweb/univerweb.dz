f78d519c37b72676346050c01b9040e97cd09457
Houssem Nedjar <h.nedjar@univerweb.dz>
2020-03-26 18:31:34 +0100

    Add Toggler icon

--- components/header/Toggler.vue
+++ components/header/Toggler.vue
@@ -1,31 +1,195 @@
 <template>
+  <!-- <div> -->
+  <!-- <button
+      type="button"
+      aria-label="Ouvrir le menu"
+      class="toggler btn"
+      @click.prevent="$store.commit('open')"
+    >
+      Menu
+    </button> -->
+
   <button
     type="button"
     aria-label="Ouvrir le menu"
-    class="toggler btn"
+    class="toggler"
     @click.prevent="$store.commit('open')"
   >
-    Menu
+    <span class="toggler-icon"></span>
   </button>
+  <!-- </div> -->
 </template>
 
 <script></script>
 
 <style>
-.toggler.btn {
+.toggler {
+  background: transparent;
+  width: 25px;
   height: 25px;
-  margin: 15px 0;
-  padding: 0 10px;
-  font-size: 13px;
-}
-
-.menu--opened .toggler {
   margin: 0;
+  padding: 0;
+  text-align: left;
 }
 
 @media (min-width: 1024px) {
-  .toggler.btn {
+  .toggler {
     display: none;
   }
+}
+
+.toggler:focus {
+  outline: none;
+}
+
+.toggler-icon {
+  display: inline-block;
+  width: 1.5em;
+  height: 1.5em;
+  vertical-align: middle;
+  content: '';
+  background: no-repeat center center;
+  background-size: 100% 100%;
+}
+
+.toggler-icon,
+.toggler-icon:before,
+.toggler-icon:after {
+  background: var(--blue);
+  height: 3px;
+  border-radius: 3px;
+  transition: all 0.3s;
+}
+
+.toggler-icon:before,
+.toggler-icon:after {
+  content: '';
+  display: block;
+}
+
+@keyframes CollapseCenter {
+  0% {
+    width: 0;
+  }
+  75% {
+    width: 0;
+  }
+  100% {
+    width: 15px;
+  }
+}
+
+.toggler-icon {
+  animation: CollapseCenter 0.5s forwards;
+}
+
+@keyframes CollapseShowCenter {
+  0% {
+    width: 15px;
+  }
+  25% {
+    width: 0;
+  }
+  100% {
+    width: 0;
+  }
+}
+
+.opened .toggler .toggler-icon {
+  animation: CollapseShowCenter 0.5s forwards;
+}
+
+@keyframes CollapseBefore {
+  0% {
+    transform: translateY(0) rotate(-45deg);
+    width: 25px;
+  }
+  25% {
+    transform: translateY(0) rotate(0);
+  }
+  50% {
+    transform: translateY(-10px) rotate(0);
+  }
+  100% {
+    transform: translateY(-10px) rotate(0);
+    width: 25px;
+  }
+}
+
+.toggler-icon:before {
+  animation: CollapseBefore 0.5s forwards;
+}
+
+@keyframes CollapseShowBefore {
+  0% {
+    transform: translateY(-10px) rotate(0);
+    width: 25px;
+  }
+  50% {
+    transform: translateY(-10px) rotate(0);
+  }
+  75% {
+    transform: translateY(0) rotate(0);
+  }
+  100% {
+    transform: translateY(0) rotate(-45deg);
+    width: 25px;
+  }
+}
+
+.opened .toggler .toggler-icon:before {
+  animation: CollapseShowBefore 0.5s forwards;
+}
+
+@keyframes CollapseAfter {
+  0% {
+    transform: translateY(-3px) rotate(45deg);
+    width: 25px;
+  }
+  25% {
+    transform: translateY(-3px) rotate(0);
+  }
+  50% {
+    transform: translateY(7px) rotate(0);
+    width: 25px;
+  }
+  75% {
+    transform: translateY(7px) rotate(0);
+    width: 20px;
+  }
+  100% {
+    transform: translateY(7px) rotate(0);
+    width: 20px;
+  }
+}
+
+.toggler-icon:after {
+  animation: CollapseAfter 0.5s forwards;
+}
+
+@keyframes CollapseShowAfter {
+  0% {
+    transform: translateY(7px) rotate(0);
+    width: 20px;
+  }
+  25% {
+    transform: translateY(7px) rotate(0);
+    width: 20px;
+  }
+  50% {
+    transform: translateY(7px) rotate(0);
+    width: 25px;
+  }
+  75% {
+    transform: translateY(-3px) rotate(0);
+  }
+  100% {
+    transform: translateY(-3px) rotate(45deg);
+    width: 25px;
+  }
+}
+
+.opened .toggler .toggler-icon:after {
+  animation: CollapseShowAfter 0.5s forwards;
 }
 </style>
