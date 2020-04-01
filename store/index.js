6528eaebfaca4fb0332d052164c559a97873641e
Houssem Nedjar <h.nedjar@univerweb.dz>
2020-04-01 15:22:27 +0100

    Update Works Page

--- store/index.js
+++ store/index.js
@@ -1,12 +1,18 @@
 export const state = () => ({
-  isOpen: false
+  isOpen: false,
+
+  works: []
 })
 
 export const getters = {}
 
 export const actions = {
   open: ({ commit }) => commit('open'),
-  close: ({ commit }) => commit('close')
+  close: ({ commit }) => commit('close'),
+
+  async all({ commit }, works) {
+    await commit('SET_POST', works)
+  }
 }
 
 export const mutations = {
@@ -16,5 +22,7 @@ export const mutations = {
 
   close(state) {
     state.isOpen = false
-  }
+  },
+
+  SET_POST: (state, works) => (state.works = works)
 }
