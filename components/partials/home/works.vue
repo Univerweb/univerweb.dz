6cd9c8b9e66c2cbdb7101d4e8f3b0177c76eb2f7
Houssem Nedjar <h.nedjar@univerweb.dz>
2020-04-25 18:03:12 +0100

    Update work.vue

--- components/partials/home/works.vue
+++ components/partials/home/works.vue
@@ -19,27 +19,25 @@
   </div>
 
   <!-- <div class="grid">
-        <div v-for="(work, index) in works" :key="index" class="item">
-          <h2 class="h3">
-            <nuxt-link
-              :to="
-                localePath({
-                  name: 'realisations-id',
-                  params: { id: work.slug }
-                })
-              "
-            >
-              {{ work.name }}
-            </nuxt-link>
-          </h2>
-          <nuxt-link
-            :to="
-              localePath({ name: 'realisations-id', params: { id: work.slug } })
-            "
-          >
-            <img :src="work.thumbnail" :alt="work.name" />
-            <p>{{ work.description }}</p>
-          </nuxt-link>
-        </div>
-      </div> -->
+    <div v-for="(work, index) in works" :key="index" class="item">
+      <h2 class="h3">
+        <nuxt-link
+          :to="
+            localePath({
+              name: 'realisations-id',
+              params: { id: work.slug }
+            })
+          "
+        >
+          {{ work.name }}
+        </nuxt-link>
+      </h2>
+      <nuxt-link
+        :to="localePath({ name: 'realisations-id', params: { id: work.slug } })"
+      >
+        <img :src="work.thumbnail" :alt="work.name" />
+        <p>{{ work.description }}</p>
+      </nuxt-link>
+    </div>
+  </div> -->
 </template>
