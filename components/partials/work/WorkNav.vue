94a15363538ae971a27fd9a3f95cd9bbffa61a1c
Houssem Nedjar <h.nedjar@univerweb.dz>
2020-06-14 16:34:19 +0100

    Add WorkNav component

---
+++ components/partials/work/WorkNav.vue
@@ -0,0 +1,120 @@
+<template>
+  <div class="nav">
+    <div class="container grid">
+      <nuxt-link
+        v-if="currentIndex < count - 1"
+        :to="localePath('/realisations/' + previus[0].slug)"
+        class="link previous"
+        :data-text="$t('links.previous')"
+      >
+        <nav-arrow />
+        {{ previus[0].title }}
+      </nuxt-link>
+
+      <nuxt-link
+        v-if="currentIndex > 0"
+        :to="localePath('/realisations/' + next[0].slug)"
+        class="link next"
+        :data-text="$t('links.next')"
+      >
+        {{ next[0].title }}
+        <nav-arrow />
+      </nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+import NavArrow from '@/assets/icons/arrow.svg?inline'
+
+export default {
+  components: {
+    NavArrow
+  },
+
+  data() {
+    const works = this.$t('worksItem')
+    const slug = this.$route.params.slug
+    const work = works.find((work) => work.slug === slug)
+    const count = works.length
+    const currentIndex = works.indexOf(work)
+    const next = works.slice(currentIndex - 1, currentIndex)
+    const previus = works.slice(currentIndex + 1, currentIndex + 2)
+    const data = {
+      next,
+      previus,
+      currentIndex,
+      count
+    }
+    return data
+  }
+}
+</script>
+
+<style lang="scss">
+.nav {
+  background: $blue;
+  & .grid {
+    grid-template-columns: repeat(2, auto);
+    padding-top: 70px;
+    padding-bottom: 45px;
+  }
+  & .next,
+  & .previous {
+    position: relative;
+    color: $dark-blue;
+    [lang='ar'] & {
+      font-family: $font-arabe;
+    }
+    &:hover {
+      color: $white;
+    }
+    &::before {
+      content: attr(data-text);
+      position: absolute;
+      color: $dark-blue;
+      top: -15px;
+      left: 40px;
+      [lang='ar'] & {
+        left: 0;
+        right: 40px;
+      }
+      font-size: 0.8rem;
+      pointer-events: none;
+    }
+    &:not(.previous) {
+      grid-column: 2;
+      text-align: right;
+      &::before {
+        right: 40px;
+        [lang='ar'] & {
+          left: 40px;
+          text-align: left;
+        }
+      }
+    }
+  }
+  & .arrow {
+    fill: currentColor;
+    height: 13px;
+    transform: rotate(90deg);
+    margin-right: 10px;
+    margin-bottom: 2px;
+    [lang='ar'] & {
+      transform: rotate(-90deg);
+      margin-right: 0;
+      margin-left: 10px;
+    }
+  }
+  & .next .arrow {
+    margin-right: 0;
+    margin-left: 10px;
+    transform: rotate(-90deg);
+    [lang='ar'] & {
+      transform: rotate(90deg);
+      margin-right: 10px;
+      margin-left: 0;
+    }
+  }
+}
+</style>
--- pages/realisations/_slug.vue
+++ pages/realisations/_slug.vue
@@ -7,42 +7,20 @@
     <div class="card">
       <img v-lazy="'/works/' + work.slug + '.jpg'" :alt="work.title" />
     </div>
-    <div class="nav">
-      <div class="container grid">
-        <nuxt-link
-          v-if="currentIndex < count - 1"
-          :to="localePath('/realisations/' + previus[0].slug)"
-          class="link previous"
-          :data-text="$t('links.previous')"
-        >
-          <work-arrow />
-          {{ previus[0].title }}
-        </nuxt-link>
-
-        <nuxt-link
-          v-if="currentIndex > 0"
-          :to="localePath('/realisations/' + next[0].slug)"
-          class="link next"
-          :data-text="$t('links.next')"
-        >
-          {{ next[0].title }}
-          <work-arrow />
-        </nuxt-link>
-      </div>
-    </div>
+    <work-nav />
     <work-request />
   </main>
 </template>
 
 <script>
-import WorkArrow from '@/assets/icons/arrow.svg?inline'
 import WorkBack from '@/components/partials/work/WorkBack'
+import WorkNav from '@/components/partials/work/WorkNav'
 import WorkRequest from '@/components/common/AppRequest'
 
 export default {
   components: {
-    WorkArrow,
     WorkBack,
+    WorkNav,
     WorkRequest
   },
 
@@ -56,18 +34,7 @@ export default {
     const works = this.$t('worksItem')
     const slug = this.$route.params.slug
     const work = works.find((work) => work.slug === slug)
-    const count = works.length
-    const currentIndex = works.indexOf(work)
-    const next = works.slice(currentIndex - 1, currentIndex)
-    const previus = works.slice(currentIndex + 1, currentIndex + 2)
-    const data = {
-      work,
-      next,
-      previus,
-      currentIndex,
-      count
-    }
-    return data
+    return { work }
   },
 
   head() {
@@ -100,86 +67,4 @@ export default {
 }
 </script>
 
-<style lang="scss">
-.work {
-  & .back {
-    display: inline-block;
-    margin-bottom: 30px;
-    font-size: 0.8rem;
-    font-weight: 800;
-    text-transform: uppercase;
-    [lang='ar'] & {
-      font-family: $font-arabe;
-    }
-  }
-  & .nav {
-    background: $blue;
-  }
-  & .grid {
-    grid-template-columns: repeat(2, auto);
-    padding-top: 70px;
-    padding-bottom: 45px;
-  }
-  & .next,
-  & .previous {
-    position: relative;
-    color: $dark-blue;
-    [lang='ar'] & {
-      font-family: $font-arabe;
-    }
-    &:hover {
-      color: $white;
-    }
-    &::before {
-      content: attr(data-text);
-      position: absolute;
-      color: $dark-blue;
-      top: -15px;
-      left: 40px;
-      [lang='ar'] & {
-        left: 0;
-        right: 40px;
-      }
-      font-size: 12px;
-      font-size: 0.8rem;
-      pointer-events: none;
-    }
-    &:not(.previous) {
-      grid-column: 2;
-      text-align: right;
-      &::before {
-        right: 40px;
-        [lang='ar'] & {
-          left: 40px;
-          text-align: left;
-        }
-      }
-    }
-  }
-  & .arrow {
-    fill: currentColor;
-    height: 13px;
-    transform: rotate(90deg);
-    margin-right: 10px;
-    margin-bottom: 2px;
-    [lang='ar'] & {
-      transform: rotate(-90deg);
-      margin-right: 0;
-      margin-left: 10px;
-    }
-  }
-  & .next .arrow {
-    margin-right: 0;
-    margin-left: 10px;
-    transform: rotate(-90deg);
-    [lang='ar'] & {
-      transform: rotate(90deg);
-      margin-right: 10px;
-      margin-left: 0;
-    }
-  }
-  & .back .arrow {
-    height: 5px;
-  }
-}
-</style>
+<style lang="scss"></style>
