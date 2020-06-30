bab57453c2075bdbc3d2e48a67c65cb34776dc25
Houssem Nedjar <h.nedjar@univerweb.dz>
2020-06-30 23:01:57 +0100

    Update Prestations Page

--- pages/prestations.vue
+++ pages/prestations.vue
@@ -1,9 +1,27 @@
 <template>
   <main>
-    <div class="container hero">
+    <div class="container">
       <div class="intro">
         <h1>{{ $t('prestations.headline') }}</h1>
         <p class="lead">{{ $t('prestations.body') }}</p>
+      </div>
+    </div>
+    <div class="container webDesign">
+      <div class="intro">
+        <h2>{{ $t('prestations.webDesign.headline') }}</h2>
+        <p class="lead">{{ $t('prestations.webDesign.body') }}</p>
+      </div>
+    </div>
+    <div class="container dev">
+      <div class="intro">
+        <h2>{{ $t('prestations.dev.headline') }}</h2>
+        <p class="lead">{{ $t('prestations.dev.body') }}</p>
+      </div>
+    </div>
+    <div class="container support">
+      <div class="intro">
+        <h2>{{ $t('prestations.support.headline') }}</h2>
+        <p class="lead">{{ $t('prestations.support.body') }}</p>
       </div>
     </div>
   </main>
@@ -13,6 +31,7 @@
 export default {
   head() {
     return {
+      bodyAttrs: { class: 'prestations' },
       titleTemplate: `${this.$t('prestations.title')} — ${this.$t('name')}`,
       meta: [
         {
@@ -40,3 +59,15 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+.prestations .webDesign .intro *,
+.prestations .support .intro * {
+  @media (min-width: $sm) {
+    grid-column: 6 / 13;
+  }
+  @media (min-width: $md) {
+    grid-column: 7 / 13;
+  }
+}
+</style>
