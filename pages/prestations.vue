3bb89af1afe2f976e30ef83ca21e2431b1c2774f
Houssem Nedjar <h.nedjar@univerweb.dz>
2020-11-30 11:21:54 +0100

    Update CSS

--- pages/prestations.vue
+++ pages/prestations.vue
@@ -9,12 +9,12 @@
         <h2>{{ $t('prestations.webDesign.headline') }}</h2>
       </div>
       <div class="details">
+        <p class="item lead">{{ $t('prestations.webDesign.body') }}</p>
         <ul class="item">
           <li v-for="item in $t('prestations.webDesign.tags')" :key="item">
             {{ item }}
           </li>
         </ul>
-        <p class="item lead">{{ $t('prestations.webDesign.body') }}</p>
       </div>
     </div>
     <div class="container">
@@ -22,12 +22,12 @@
         <h2>{{ $t('prestations.dev.headline') }}</h2>
       </div>
       <div class="details">
+        <p class="item lead">{{ $t('prestations.dev.body') }}</p>
         <ul class="item">
           <li v-for="item in $t('prestations.dev.tags')" :key="item">
             {{ item }}
           </li>
         </ul>
-        <p class="item lead">{{ $t('prestations.dev.body') }}</p>
       </div>
     </div>
     <div class="container">
@@ -35,12 +35,12 @@
         <h2>{{ $t('prestations.support.headline') }}</h2>
       </div>
       <div class="details">
+        <p class="item lead">{{ $t('prestations.support.body') }}</p>
         <ul class="item">
           <li v-for="item in $t('prestations.support.tags')" :key="item">
             {{ item }}
           </li>
         </ul>
-        <p class="item lead">{{ $t('prestations.support.body') }}</p>
       </div>
     </div>
   </main>
@@ -73,42 +73,3 @@ export default {
   }
 }
 </script>
-
-<style lang="scss">
-.prestations .container:nth-child(2n) h2 {
-  @media (min-width: $sm) {
-    grid-column: 7 / 12;
-  }
-}
-
-.prestations .container {
-  .details {
-    align-items: center;
-    ul {
-      display: flex;
-      flex-wrap: wrap;
-      column-gap: 12px;
-      margin: 0;
-      padding: 0;
-      list-style: none;
-      color: $black-light;
-      font-weight: 500;
-      @media (min-width: $sm) {
-        grid-column: 3 / 6;
-      }
-    }
-  }
-  &:nth-child(3) .details ul {
-    @media (min-width: $sm) {
-      grid-column: 7 / 10;
-    }
-  }
-}
-
-.prestations .container:nth-child(3) p {
-  @media (min-width: $sm) {
-    order: -1;
-    grid-column: 1 / 6;
-  }
-}
-</style>
