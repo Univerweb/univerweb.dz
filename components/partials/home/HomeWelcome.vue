037b465a39c1c1fde6a40663fdc4a736d92d3ae1
Houssem Nedjar <h.nedjar@univerweb.dz>
2020-12-12 04:00:29 +0100

    Update(Vue): replace HomeWelcome in index.vue (suite)

---
+++ components/partials/home/HomeWelcome.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="container hero">
+    <div class="intro">
+      <h1>{{ $t('home.headline') }}</h1>
+      <p class="lead">{{ $t('home.body') }}</p>
+    </div>
+    <HomeArrow v-scroll-to="'.works'" />
+  </section>
+</template>
+
+<script>
+import HomeArrow from '@/assets/icons/arrow.svg?inline'
+
+export default {
+  components: {
+    HomeArrow
+  }
+}
+</script>
