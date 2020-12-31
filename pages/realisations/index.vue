659708f58bc71e6bd5ab89c28937a77c600c076a
Houssem Nedjar <h.nedjar@univerweb.dz>
2020-12-31 10:54:09 +0100

    Add(StructuredData): BreadcrumbList to Works Page

--- pages/realisations/index.vue
+++ pages/realisations/index.vue
@@ -19,13 +19,43 @@ export default {
     }
   },
 
-  head() {
+  head({ $config: { baseURL } }) {
+    let routeItem = `${baseURL}/`
+    if (this.$i18n.locale !== 'fr') {
+      routeItem = `${baseURL}/${this.$i18n.locale}`
+    }
+
     return {
       titleTemplate: `${this.title} — ${this.$t('name')}`,
+
       meta: [
         { hid: 'description', name: 'description', content: this.description },
         { hid: 'og:title', property: 'og:title', content: this.title },
         { hid: 'og:description', property: 'og:description', content: this.description }
+      ],
+
+      script: [
+        {
+          json: {
+            '@context': 'https://schema.org',
+            '@type': 'BreadcrumbList',
+            itemListElement: [
+              {
+                '@type': 'ListItem',
+                position: 1,
+                name: this.$t('name'),
+                item: routeItem
+              },
+              {
+                '@type': 'ListItem',
+                position: 2,
+                name: this.title,
+                item: `${baseURL}${this.$route.fullPath}`
+              }
+            ]
+          },
+          type: 'application/ld+json'
+        }
       ]
     }
   }
