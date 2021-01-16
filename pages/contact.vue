20304d0395f782cda333b9c78243a73ab0962d4c
Houssem Nedjar <h.nedjar@univerweb.dz>
2021-01-16 15:15:09 +0100

    Update(StructuredData): BreadcrumbList to Contact Page

--- pages/contact.vue
+++ pages/contact.vue
@@ -37,10 +37,10 @@ export default {
     }
   },
 
-  head({ $config: { baseURL } }) {
-    let routeItem = `${baseURL}/`
+  head() {
+    let routeItem = `${this.$config.baseURL}/`
     if (this.$i18n.locale !== 'fr') {
-      routeItem = `${baseURL}/${this.$i18n.locale}`
+      routeItem = `${this.$config.baseURL}/${this.$i18n.locale}`
     }
 
     return {
@@ -57,6 +57,7 @@ export default {
           json: {
             '@context': 'https://schema.org',
             '@type': 'BreadcrumbList',
+            name: this.title,
             itemListElement: [
               {
                 '@type': 'ListItem',
@@ -67,8 +68,7 @@ export default {
               {
                 '@type': 'ListItem',
                 position: 2,
-                name: this.title,
-                item: `${baseURL}${this.$route.fullPath}`
+                name: this.title
               }
             ]
           },
