94194a347040a91ca1ea6b65af2e1f1d564afdfc
Houssem Nedjar <h.nedjar@univerweb.dz>
2021-01-16 15:14:45 +0100

    Update(StructuredData): BreadcrumbList to Agence Page

--- pages/agence.vue
+++ pages/agence.vue
@@ -45,10 +45,10 @@ export default {
     }
   },
 
-  head({ $config: { baseURL } }) {
-    let routeItem = `${baseURL}/`
+  head() {
+    let routeItem = `${this.$config.baseURL}/`
     if (this.$i18n.locale !== 'fr') {
-      routeItem = `${baseURL}/${this.$i18n.locale}`
+      routeItem = `${this.$config.baseURL}/${this.$i18n.locale}`
     }
 
     return {
@@ -65,6 +65,7 @@ export default {
           json: {
             '@context': 'https://schema.org',
             '@type': 'BreadcrumbList',
+            name: this.title,
             itemListElement: [
               {
                 '@type': 'ListItem',
@@ -75,8 +76,7 @@ export default {
               {
                 '@type': 'ListItem',
                 position: 2,
-                name: this.title,
-                item: `${baseURL}${this.$route.fullPath}`
+                name: this.title
               }
             ]
           },
