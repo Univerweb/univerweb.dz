46aa2f72d2aecc59f140415a67a2ba7b7d27fa0b
Houssem <h.nedjar@univerweb.dz>
2022-12-23 18:44:34 +0100

    update: Running AppAbout

--- components/app/AppAbout.vue
+++ components/app/AppAbout.vue
@@ -1,9 +1,13 @@
 <template>
   <div class="about">
-    <p class="lead">{{ $t('footer.about') }}</p>
-    <a :href="`mailto:${$config.public.baseEmail}`" class="link"
-      ><span class="visually-hidden">{{ $t('label.email') }}</span
-      >{{ $config.public.baseEmail }}</a
-    >
+    <p class="lead">
+      <!-- {{ $t('footer.about') }} -->
+    </p>
+    <a :href="`mailto:${$config.public.baseEmail}`" class="link">
+      <span class="visually-hidden">
+        <!-- {{ $t('label.email') }} -->
+      </span>
+      {{ $config.public.baseEmail }}
+    </a>
   </div>
 </template>
