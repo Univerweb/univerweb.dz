ffc7e13668110a56b9b4284210df809c2a355986
Houssem <h.nedjar@univerweb.dz>
2022-12-26 11:30:31 +0100

    update: header

---
+++ components/header/HeaderLogo.vue
@@ -0,0 +1,30 @@
+<template>
+  <NuxtLink :to="localePath('/')" :title="t('name')" class="logo">
+    <span class="visually-hidden">{{ t('name') }}</span>
+    <!-- <NuxtIcon name="logos/univerweb-ar" />
+    <NuxtIcon name="logos/univerweb" /> -->
+  </NuxtLink>
+</template>
+
+<script setup lang="ts">
+const localePath = useLocalePath()
+const { t, locale } = useI18n()
+
+const lang = locale.value === 'ar'
+</script>
+
+<style lang="scss">
+.logo svg {
+  display: block;
+  g:first-child {
+    fill: $primary;
+  }
+  g:last-child {
+    fill: $secondary;
+    transition: fill $transition;
+    .dark-mode & {
+      fill: $white;
+    }
+  }
+}
+</style>
---
+++ components/header/HeaderToggle.vue
@@ -0,0 +1,57 @@
+<template>
+  <button type="button" :aria-label="t('label.menu')" class="toggle" @click="isShow">
+    <span class="top"></span>
+    <span class="middle"></span>
+    <span class="bottom"></span>
+  </button>
+</template>
+
+<script setup lang="ts">
+const { t } = useI18n()
+
+const show = ref(false)
+const isShow = () => (show.value = !show.value)
+</script>
+
+<style lang="scss">
+.toggle {
+  display: grid;
+  @media (min-width: $lg) {
+    display: none;
+  }
+  align-content: space-between;
+  background-color: transparent;
+  width: 24px;
+  height: 24px;
+  margin: 0;
+  padding: 0;
+  outline: none;
+  span {
+    background-color: $primary;
+    height: 3px;
+    border-radius: 2em;
+    transition: all $transition;
+  }
+  .middle {
+    animation: collapse-middle $animation;
+    transform-origin: left;
+    .show & {
+      animation: collapse-show-middle $animation;
+    }
+  }
+  .top {
+    animation: collapse-top $animation;
+    .show & {
+      animation: collapse-show-top $animation;
+    }
+  }
+  .bottom {
+    animation: collapse-bottom $animation;
+    transform-origin: left;
+    .show & {
+      animation: collapse-show-bottom $animation;
+      transform-origin: center;
+    }
+  }
+}
+</style>
