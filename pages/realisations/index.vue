58fa1c4f6d55ddd62af13ded0dcbbe57d48fcaa5
Houssem <h.nedjar@univerweb.dz>
2022-12-26 17:50:26 +0100

    upgrade: realisations

--- pages/realisations/index.vue
+++ pages/realisations/index.vue
@@ -1,103 +1,50 @@
 <template>
   <main>
-    <!-- <AppWorks :headline="headline" :works="works" />
-    <AppRequest /> -->
+    <AppWorks :headline="headline" :works="works!" />
+    <AppRequest />
   </main>
 </template>
 
 <script setup lang="ts">
-const title = ref('My App')
-const description = ref('My App Description')
-const name = ref('Name')
-const item = ref('Item')
+const { locale } = useI18n()
+const config = useRuntimeConfig()
+
+const worksPagePath = `${locale.value}/works`
+const worksPath = `${locale.value}/works_slug`
+const globalPath = `${locale.value}/global`
+const baseURL = config.public.baseURL
+
+const { data: worksPage } = await useAsyncData('worksPage', () => queryContent(worksPagePath).only(['title', 'desc', 'headline']).findOne())
+const { data: works } = await useAsyncData('works', () => queryContent(worksPath).only(['title', 'tags', 'lead']).sort({ _id: -1 }).find())
+const { data: global } = await useAsyncData('global', () => queryContent(globalPath).only(['name']).findOne())
+
+const title = worksPage.value!.title
+const desc = worksPage.value!.desc
+const headline = worksPage.value!.headline
+const name = global.value!.name
+const item = locale.value !== 'fr' ? `${baseURL}/${locale.value}` : `${baseURL}/`
 
 useHead({
   title,
+
   meta: [
-    {
-      name: 'description',
-      content: description
-    },
-    {
-      property: 'og:title',
-      content: title
-    },
-    {
-      property: 'og:description',
-      content: description
-    }
+    { name: 'description', content: desc },
+    { property: 'og:title', content: title },
+    { property: 'og:description', content: desc }
   ],
+
   script: [
     {
-      json: {
+      type: 'application/ld+json',
+      children: {
         '@context': 'https://schema.org',
         '@type': 'BreadcrumbList',
         itemListElement: [
           { '@type': 'ListItem', position: 1, name: name, item: item },
           { '@type': 'ListItem', position: 2, name: title }
         ]
-      },
-      type: 'application/ld+json'
+      }
     }
   ]
 })
 </script>
-
-<!-- <script>
-export default {
-  name: 'RealisationsPage',
-
-  async asyncData({ $content, app }) {
-    const { title, description, headline } = await $content(app.i18n.locale, 'works').only(['title', 'description', 'headline']).fetch()
-    const works = await $content(app.i18n.locale, 'works_slug').only(['slug', 'title', 'description', 'tags', 'lead']).sortBy('position', 'desc').fetch()
-
-    return {
-      title,
-      description,
-      headline,
-      works
-    }
-  }
-
-  // OLD Code
-  // head() {
-  //   let routeItem = `${this.$config. public.baseURL}/`
-  //   if (this.$i18n.locale !== 'fr') {
-  //     routeItem = `${this.$config. public.baseURL}/${this.$i18n.locale}`
-  //   }
-
-  //   return {
-  //     title: this.title,
-
-  //     meta: [
-  //       { hid: 'description', name: 'description', content: this.description },
-  //       { hid: 'og:title', property: 'og:title', content: this.title },
-  //       { hid: 'og:description', property: 'og:description', content: this.description }
-  //     ],
-
-  //     script: [
-  //       {
-  //         json: {
-  //           '@context': 'https://schema.org',
-  //           '@type': 'BreadcrumbList',
-  //           itemListElement: [
-  //             {
-  //               '@type': 'ListItem',
-  //               position: 1,
-  //               name: this.$t('name'),
-  //               item: routeItem
-  //             },
-  //             {
-  //               '@type': 'ListItem',
-  //               position: 2,
-  //               name: this.title
-  //             }
-  //           ]
-  //         },
-  //         type: 'application/ld+json'
-  //       }
-  //     ]
-  //   }
-  // }
-}
-</script> -->
