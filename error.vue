4dd8125474f305bea27765fb05ce2ce19ee29b6b
Houssem <h.nedjar@univerweb.dz>
2023-01-04 18:38:12 +0100

    update: lang

--- error.vue
+++ error.vue
@@ -1,20 +1,19 @@
 <template>
   <main class="container hero error">
     <Head>
-      <Title>{{ error.statusCode }} — {{ global!.name }}</Title>
+      <Title>{{ error.statusCode }} — {{ t('name') }}</Title>
       <Meta name="robots" :content="'noindex, follow'" />
     </Head>
 
-    <h1>{{ global!.error.headline }}</h1>
-    <p v-if="error.statusCode == 404" class="lead">{{ global!.error.error404 }}</p>
-    <p v-else class="lead">{{ global!.error.error500 }}</p>
-    <button class="btn" @click="handleError">{{ global!.error.back }}</button>
+    <h1>{{ t('error.headline') }}</h1>
+    <p v-if="error.statusCode == 404" class="lead">{{ t('error.error404') }}</p>
+    <p v-else class="lead">{{ t('error.error500') }}</p>
+    <button class="btn" @click="handleError">{{ t('error.back') }}</button>
   </main>
 </template>
 
 <script setup lang="ts">
-const { locale } = useI18n()
-const { data: global } = await useAsyncData('errorGlobal', () => queryContent(locale.value, 'global').only(['name', 'error']).findOne())
+const { t } = useI18n()
 
 defineProps({
   error: {
