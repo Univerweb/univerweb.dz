5ef95d50ee7c062517e7de17c83cea20b85d33d6
Houssem <h.nedjar@univerweb.dz>
2023-01-06 15:23:55 +0100

    update: HomeWelcome lang

--- components/home/HomeWelcome.vue
+++ components/home/HomeWelcome.vue
@@ -1,22 +1,13 @@
 <template>
   <section class="container hero">
     <div class="intro">
-      <h1>{{ headline }}</h1>
-      <p class="lead">{{ lead }}</p>
+      <h1>{{ t('home.headline') }}</h1>
+      <p class="lead">{{ t('home.lead') }}</p>
     </div>
     <!-- <NuxtIcon name="arrow" v-scroll-to="'.works'" /> -->
   </section>
 </template>
 
 <script setup lang="ts">
-defineProps({
-  headline: {
-    type: String,
-    required: true
-  },
-  lead: {
-    type: String,
-    required: true
-  }
-})
+const { t } = useI18n()
 </script>
