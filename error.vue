25aaa5ffbec0971c61812a9fa0bf82b44c3f8ca5
Houssem <h.nedjar@univerweb.dz>
2023-01-08 09:13:58 +0100

    fix: eslint

--- error.vue
+++ error.vue
@@ -1,20 +1,4 @@
-<template>
-  <main class="container hero error">
-    <Head>
-      <Title>{{ error.statusCode }} — {{ t('name') }}</Title>
-      <Meta name="robots" :content="'noindex, follow'" />
-    </Head>
-
-    <h1>{{ t('error.headline') }}</h1>
-    <p v-if="error.statusCode == 404" class="lead">{{ t('error.error404') }}</p>
-    <p v-else class="lead">{{ t('error.error500') }}</p>
-    <button class="btn" @click="handleError">{{ t('error.back') }}</button>
-  </main>
-</template>
-
 <script setup lang="ts">
-const { t } = useI18n()
-
 defineProps({
   error: {
     type: Object,
@@ -22,8 +6,32 @@ defineProps({
   }
 })
 
+const { t } = useI18n()
+
 const handleError = () => clearError({ redirect: '/' })
 </script>
+
+<template>
+  <main class="container hero error">
+    <Head>
+      <Title>{{ error.statusCode }} — {{ t('name') }}</Title>
+      <Meta name="robots" content="noindex, follow" />
+    </Head>
+
+    <h1>
+      {{ t('error.headline') }}
+    </h1>
+    <p v-if="error.statusCode === 404" class="lead">
+      {{ t('error.error404') }}
+    </p>
+    <p v-else class="lead">
+      {{ t('error.error500') }}
+    </p>
+    <button class="btn" @click="handleError">
+      {{ t('error.back') }}
+    </button>
+  </main>
+</template>
 
 <style lang="scss" scoped>
 .btn {
