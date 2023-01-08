6fa979cae26be915cdd54211067d10f40105065f
Houssem <h.nedjar@univerweb.dz>
2023-01-08 09:37:12 +0100

    fix: eslint

--- components/app/AppRequest.vue
+++ components/app/AppRequest.vue
@@ -10,6 +10,17 @@ const { t } = useI18n()
 const localePath = useLocalePath()
 </script>
 
+<template>
+  <div class="container request">
+    <h2 class="h3">
+      {{ t('request.headline') }}
+    </h2>
+    <NuxtLink :to="localePath('contact')" :class="home" class="move-arrow">
+      {{ t('request.link') }}
+    </NuxtLink>
+  </div>
+</template>
+
 <style lang="scss" scoped>
 .request {
   background-color: var(--secondary);
@@ -22,10 +33,3 @@ const localePath = useLocalePath()
   }
 }
 </style>
-
-<template>
-  <div class="container request">
-    <h2 class="h3">{{ t('request.headline') }}</h2>
-    <NuxtLink :to="localePath('contact')" :class="home" class="move-arrow">{{ t('request.link') }}</NuxtLink>
-  </div>
-</template>
