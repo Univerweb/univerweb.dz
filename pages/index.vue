8bf9fa853638ca9a5d34ed71b888af789edc4ef8
Houssem <h.nedjar@univerweb.dz>
2023-01-12 18:27:38 +0100

    update: WorkList

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,7 @@
-<script setup lang="ts">
-const { locale } = useI18n()
-
-const { data: works } = await useAsyncData('HomeWorks', () =>
-  queryContent(locale.value, 'realisations').only(['headline', 'title', 'tags', 'lead', '_path']).limit(6).sort({ _id: -1 }).find(),
-)
-</script>
-
 <template>
-  <main v-if="works">
+  <main>
     <AppWelcome />
-    <WorkList :works="works" h1="h2" like-h1="h1" h2="h3" more="div" />
+    <WorkList :limit="6" h1="h2" like-h1="h1" h2="h3" more="div" />
     <AppRequest home="h1" />
   </main>
 </template>
