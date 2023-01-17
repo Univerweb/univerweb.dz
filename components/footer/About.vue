87f8f7994ed1a0f3e24543231f0d6f344552b0ed
Houssem <h.nedjar@univerweb.dz>
2023-01-17 10:42:12 +0100

    update: footer

---
+++ components/footer/About.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const { t } = useI18n()
+</script>
+
+<template>
+  <p class="lead">
+    {{ t('contact.about') }}
+  </p>
+</template>
+
+<style lang="scss">
+footer .lead {
+  color: var(--textPrimary);
+  transition: color $transition;
+  margin: 0;
+  max-width: 400px;
+}
+</style>
---
+++ components/footer/Link.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const seo = useSeo()
+const { t } = useI18n()
+</script>
+
+<template>
+  <a :href="`mailto:${seo.email}`" class="link">
+    <span class="visually-hidden">{{ t('email') }}</span>
+    {{ seo.email }}
+  </a>
+</template>
