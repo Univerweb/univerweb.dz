a33288386a990f19c6c988d60b8252b7ce3c2f95
Houssem <h.nedjar@univerweb.dz>
2023-01-30 08:28:45 +0100

    add: GA4

---
+++ plugins/vue-gtag.client.ts
@@ -0,0 +1,9 @@
+import VueGtag, { trackRouter } from 'vue-gtag-next'
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.vueApp.use(VueGtag, {
+    property: {
+      id: 'G-J69WZ5DH50',
+    },
+  })
+  trackRouter(useRouter())
+})
