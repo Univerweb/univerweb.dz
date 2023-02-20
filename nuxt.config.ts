2fa592ea74fc1473503cb4a08bf070d903312038
Houssem <h.nedjar@univerweb.dz>
2023-02-20 09:00:19 +0100

    update: nuxt.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -35,8 +35,8 @@ export default defineNuxtConfig({
   ],
 
   image: {
-    // staticFilename: 'images/[name]-[hash][ext]',
     // dir: 'assets/images',
+    // staticFilename: 'images/[name]-[hash][ext]',
   },
 
   colorMode: {
