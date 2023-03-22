4296c77b6b1b81084bb5df96813e51087dba2ec3
Houssem <h.nedjar@univerweb.dz>
2023-03-22 11:46:45 +0100

    add: blog page

---
+++ components/blog/List.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const { t } = useI18n()
+</script>
+
+<template>
+  <section class="container works">
+    <div class="intro">
+      <h1>
+        {{ t('blog.headline') }}
+      </h1>
+    </div>
+  </section>
+</template>
---
+++ pages/blog/index.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+const { t } = useI18n()
+const seo = useSeo()
+
+useHead({
+  title: t('blog.title'),
+
+  meta: [
+    { name: 'description', content: t('blog.desc') },
+    { property: 'og:title', content: t('blog.title') },
+    { property: 'og:description', content: t('blog.desc') },
+  ],
+
+  script: [
+    {
+      type: 'application/ld+json',
+      children: {
+        '@context': 'https://schema.org',
+        '@type': 'BreadcrumbList',
+        'itemListElement': [
+          { '@type': 'ListItem', 'position': 1, 'name': t('name'), 'item': seo.breadcrumbItemOne },
+          { '@type': 'ListItem', 'position': 2, 'name': t('blog.title') },
+        ],
+      },
+    },
+  ],
+})
+</script>
+
+<template>
+  <main>
+    <BlogList />
+    <LazyAppRequest />
+  </main>
+</template>
