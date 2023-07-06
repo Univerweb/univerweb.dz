9f293990f5b02cb74f51889313cd49f6f00c2bb3
Houssem Nedjar <h.nedjar@univerweb.dz>
2023-07-06 18:06:20 +0100

    add: AppList

---
+++ components/app/List.vue
@@ -0,0 +1,164 @@
+<script setup lang="ts">
+import type { Post } from '../../types'
+
+export interface Props {
+  slug?: string
+  limit?: number
+  headlineTag?: string
+  headline?: string
+  listItem?: string
+  more?: boolean
+  titleTag?: string
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  slug: 'realisations',
+  limit: 0,
+  headlineTag: 'h1',
+  headline: 'works.headline',
+  listItem: 'WorkListItem',
+  titleTag: 'h2',
+  more: false,
+})
+
+const { locale, t } = useI18n()
+const localePath = useLocalePath()
+
+const { data: posts } = await useAsyncData(
+  props.slug,
+  () => queryContent<Post>(props.slug, locale.value).sort({ _id: -1 }).limit(props.limit).find(),
+)
+</script>
+
+<template>
+  <section :id="slug" class="container">
+    <div class="intro">
+      <Component :is="headlineTag" class="h1">
+        {{ t(headline) }}
+      </Component>
+    </div>
+
+    <div v-if="posts?.length" class="details details-card">
+      <Component :is="listItem" v-for="post in posts" :key="post.slug" :post="post" :title-tag="titleTag" />
+    </div>
+
+    <div v-if="more" class="more">
+      <NuxtLink :to="localePath(slug)" class="btn">
+        {{ t('home.more') }}
+        <NuxtIcon name="arrow" />
+      </NuxtLink>
+    </div>
+  </section>
+</template>
+
+<style lang="scss">
+.details-card {
+  grid-template-columns: 1fr;
+
+  @media (min-width: $sm) {
+    grid-template-columns: 1fr 1fr;
+  }
+
+  gap: 12px;
+
+  @media (min-width: $md) {
+    gap: 16px;
+  }
+
+  @media (min-width: $lg) {
+    gap: 24px;
+  }
+
+  .item {
+    grid-column: initial;
+    overflow: hidden;
+    z-index: 1;
+    border-radius: 12px;
+
+    @media (min-width: $xl) {
+      border-radius: 24px;
+    }
+
+    article {
+      display: grid;
+      position: relative;
+    }
+
+    .overlay {
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      background-color: $text-primary;
+      opacity: 0.1;
+      transition: opacity $transition;
+    }
+
+    &:hover .overlay {
+      opacity: 0.6;
+    }
+
+    .inner {
+      display: grid;
+      grid-auto-rows: auto 1fr;
+      position: absolute;
+      height: 100%;
+      color: $white;
+      padding: 24px;
+
+      @media (min-width: $md) {
+        padding: 24px;
+      }
+
+      @media (min-width: $lg) {
+        padding: 36px;
+        grid-auto-rows: auto auto 1fr;
+      }
+
+      @media (min-width: $xl) {
+        padding: 48px;
+      }
+    }
+
+    h2,
+    h3 {
+      @include size(32);
+      margin-bottom: 12px;
+    }
+
+    p {
+      display: none;
+
+      @media (min-width: $lg) {
+        display: block;
+      }
+
+      color: $white;
+      align-self: end;
+      transform: translateY(calc(100% + 48px));
+      transition: transform $transition;
+    }
+
+    &:hover p {
+      transform: translateY(0);
+    }
+  }
+}
+
+.more {
+  display: grid;
+  margin-top: 48px;
+
+  .btn {
+    justify-self: end;
+
+    .arrow {
+      height: 8px;
+      transform: rotate(-90deg);
+
+      [lang='ar-DZ'] & {
+        transform: rotate(90deg);
+      }
+    }
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <main>
     <AppWelcome />
-    <LazyWorkList id="realisations" headline-tag="h2" title-tag="h3" :limit="6" more />
+    <LazyAppList headline-tag="h2" title-tag="h3" :limit="6" more />
     <LazyAppRequest class-name="h1" />
   </main>
 </template>
