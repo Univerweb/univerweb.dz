0028f46c65aa4ec9c0692e658bfcb46a021a3aeb
Houssem Nedjar <h.nedjar@univerweb.dz>
2023-09-01 18:36:44 +0100

    add: AppPicture

---
+++ components/app/Picture.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+defineProps<{
+  src: string
+  alt: string
+  sizes: string
+}>()
+</script>
+
+<template>
+  <!-- format="avif,webp" -->
+  <NuxtPicture :src="src" :alt="alt" :sizes="sizes" format="avif" quality="80" loading="lazy" />
+</template>
+
+<style lang="scss" scoped>
+picture {
+  display: grid;
+
+  img {
+    width: 100%;
+    height: auto;
+  }
+}
+</style>
