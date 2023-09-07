fba9449c09990e3a67d1509f31fd792f2b5339c2
Houssem Nedjar <h.nedjar@univerweb.dz>
2023-09-07 18:04:14 +0100

    update: Open Graph

--- pages/blog/[slug].vue
+++ pages/blog/[slug].vue
@@ -30,11 +30,8 @@ useSeoMeta({
   title: post.value?.title,
   description: post.value?.description,
   ogTitle: post.value?.title,
-  ogDescription: post.value?.description,
   ogType: 'article',
   ogImage: `${seo.baseUrl}/_ipx/w_1536&f_jpg&q_80/blog/${post.value?.slug}.jpg`,
-  ogImageSecureUrl: `${seo.baseUrl}/images/share/${post.value?.slug}.jpg`,
-  ogImageAlt: `${post.value?.title} — ${post.value?.industry}`,
 })
 
 useHead({
