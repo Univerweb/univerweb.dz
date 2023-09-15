16782010d0a5b248ca9d3c96afb7ca5065671fc5
Houssem Nedjar <h.nedjar@univerweb.dz>
2023-09-15 17:42:16 +0100

    update: slug

--- pages/realisations/[slug].vue
+++ pages/realisations/[slug].vue
@@ -46,7 +46,7 @@ useHead({
 </script>
 
 <template>
-  <main v-if="data" id="main" class="work">
+  <main v-if="data" class="work">
     <article vocab="https://schema.org/" typeof="Article">
       <div property="mainEntityOfPage" typeof="WebPage">
         <meta property="id" :content="`${config.public.baseURL}${path}`">
