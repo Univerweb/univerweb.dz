8361bf261487d99aca098bf2bd7601289cad8d90
Houssem Nedjar <h.nedjar@univerweb.dz>
2023-10-07 17:29:54 +0100

    update: hero CSS

--- components/app/Welcome.vue
+++ components/app/Welcome.vue
@@ -23,8 +23,7 @@ const { t } = useI18n()
 .hero {
   display: grid;
   grid-auto-rows: 1fr auto;
-  padding-top: 0;
-  padding-bottom: 0;
+  padding-block: 0;
   min-height: calc(100vh - 96px);
 
   @media (min-width: $lg) {
