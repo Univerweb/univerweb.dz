1349c123b96b39c36a8a2920d24b17004a09754c
Houssem Nedjar <h.nedjar@univerweb.dz>
2023-10-17 16:47:52 +0100

    update: types.ts

--- types.ts
+++ types.ts
@@ -12,7 +12,10 @@ export interface Work extends Post {
 }
 
 export interface Blog extends Post {
-  author: { name: string; url: string }
+  author: {
+    name: string
+    url: string
+  }
 }
 
 export type Nav = null | Pick<Post, '_path' | 'title'>
