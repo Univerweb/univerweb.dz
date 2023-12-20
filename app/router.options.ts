220df11bff08a224dd723185816719cf7ff33223
Houssem Nedjar <h.nedjar@univerweb.dz>
2023-12-20 14:41:06 +0100

    update: use smooth behavior in AppWelcome

--- app/router.options.ts
+++ app/router.options.ts
@@ -7,6 +7,9 @@ export default <RouterConfig>{
     if (nuxtApp.$i18n && to.name !== from.name)
       await nuxtApp.$i18n.waitForPendingLocaleChange()
 
+    if (to.hash === '#realisations')
+      return { el: '#realisations', behavior: 'smooth' }
+
     return savedPosition || { top: 0 }
   },
 }
--- components/app/Welcome.vue
+++ components/app/Welcome.vue
@@ -10,11 +10,11 @@ const { t } = useI18n()
         {{ t('home.lead') }}
       </p>
     </div>
-    <a href="#realisations">
+    <NuxtLink to="#realisations">
       <svg xmlns="http://www.w3.org/2000/svg" width="20" height="12" fill="currentColor" aria-hidden="true" viewBox="0 0 20 12" role="img">
         <path d="M17.6 0 10 7.6 2.4 0 .2 2.2l7.6 7.6L10 12l2.2-2.2 7.6-7.6z" />
       </svg>
       <span class="visually-hidden">{{ t('menu.realisations') }}</span>
-    </a>
+    </NuxtLink>
   </section>
 </template>
