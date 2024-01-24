5798783093dd8af73b51d244527cd7f412f5b568
Houssem Nedjar <h.nedjar@univerweb.dz>
2024-01-24 14:19:25 +0100

    fix: WorkCardGroup

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <main>
     <AppWelcome />
-    <AppCardGroup headline-tag="h2" title-tag="h3" :limit="6" more />
+    <WorkCardGroup headline-tag="h2" title-tag="h3" :limit="6" more />
     <LazyAppRequest class-name="h1" />
   </main>
 </template>
