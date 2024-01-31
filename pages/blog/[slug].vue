a5a81bbbca8347de0c078de21a02cf778632af5b
Houssem Nedjar <h.nedjar@univerweb.dz>
2024-01-31 19:53:51 +0100

    update: slug blog CSS

--- pages/blog/[slug].vue
+++ pages/blog/[slug].vue
@@ -259,22 +259,6 @@ useHead({
         }
       }
     }
-
-    sup {
-      position: relative;
-      vertical-align: text-top;
-      top: -5px;
-      font-size: 11px;
-      color: $primary;
-
-      a[aria-describedby='footnote-label'] {
-        color: $primary;
-      }
-    }
-
-    .footnotes ol {
-      font-size: 14px;
-    }
   }
 }
 </style>
