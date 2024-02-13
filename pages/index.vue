b93e3e91b0341b73c213f0bf5f80d1653827616b
Houssem Nedjar <h.nedjar@univerweb.dz>
2024-02-13 19:21:44 +0100

    update: index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template>
   <main>
     <AppWelcome />
-    <WorkCardGroup headline-tag="h2" title-tag="h3" :limit="6" more />
-    <BlogCardGroup headline-tag="h2" title-tag="h3" :limit="2" more />
+    <LazyWorkCardGroup headline-tag="h2" title-tag="h3" :limit="6" more />
+    <LazyBlogCardGroup headline-tag="h2" title-tag="h3" :limit="2" more />
     <LazyAppRequest class-name="h1" />
   </main>
 </template>
