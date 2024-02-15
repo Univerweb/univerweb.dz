2cc6c9592b04eece7e09175685cbb4d6f62662eb
Houssem Nedjar <h.nedjar@univerweb.dz>
2024-02-15 22:10:56 +0100

    remove: useLocale

--- composables/useLocale.ts
+++
@@ -1,8 +0,0 @@
-export function useLocale() {
-  const { locale } = useI18n()
-
-  return {
-    isArabic: computed(() => locale.value === 'ar'),
-    isFrench: computed(() => locale.value === 'fr'),
-  }
-}
