c079e3a6568541b84fe607d9292f3ad81c6e0cb9
Houssem Nedjar <h.nedjar@univerweb.dz>
2024-02-21 10:14:18 +0100

    update: Card CSS

--- components/blog/Card.vue
+++ components/blog/Card.vue
@@ -12,48 +12,43 @@ const localePath = useLocalePath()
 </script>
 
 <template>
-  <NuxtLink :to="localePath(`${article._path}`)" class="card">
-    <article vocab="https://schema.org/" typeof="Article">
-      <span property="mainEntityOfPage" typeof="WebPage">
-        <meta property="id" :content="`${baseUrl}${localePath(`${article._path}`)}`">
-      </span>
-      <span property="publisher" typeof="Organization">
-        <meta property="name" :content="t('name')">
-        <meta property="url" :content="baseUrl">
-      </span>
-      <span v-if="article.author && article.author.name && article.author.url" property="author" typeof="Person">
-        <meta property="name" :content="article.author.name">
-        <meta property="url" :content="article.author.url">
-      </span>
-      <span v-else property="author" typeof="Organization">
-        <meta property="name" :content="t('name')">
-        <meta property="url" :content="baseUrl">
-      </span>
-      <time property="dateCreated datePublished" :datetime="article.createdAt.toString()" />
-      <time property="dateModified" :datetime="article.updatedAt.toString()" />
-      <meta property="articleSection" :content="t('blog.title')">
+  <NuxtLink :to="localePath(`${article._path}`)" class="card" vocab="https://schema.org/" typeof="Article">
+    <span property="publisher" typeof="Organization">
+      <meta property="name" :content="t('name')">
+      <meta property="url" :content="baseUrl">
+    </span>
+    <span v-if="article.author && article.author.name && article.author.url" property="author" typeof="Person">
+      <meta property="name" :content="article.author.name">
+      <meta property="url" :content="article.author.url">
+    </span>
+    <span v-else property="author" typeof="Organization">
+      <meta property="name" :content="t('name')">
+      <meta property="url" :content="baseUrl">
+    </span>
+    <time property="dateCreated datePublished" :datetime="article.createdAt.toString()" />
+    <time property="dateModified" :datetime="article.updatedAt.toString()" />
+    <meta property="articleSection" :content="t('blog.title')">
 
-      <AppPicture
-        :picture="article"
-        type="thumbnail"
-        sizes="342px xs:348px md:460px xl:584px"
-      />
+    <AppPicture
+      :picture="article"
+      type="thumbnail"
+      sizes="342px xs:348px md:460px xl:584px"
+    />
 
-      <div class="overlay" />
+    <div class="overlay" />
 
-      <div class="inner">
-        <ul>
-          <li v-for="tag in article.tags" :key="tag" property="keywords" class="h3 tag">
-            {{ tag }}
-          </li>
-        </ul>
-        <Component :is="titleTag" property="headline">
-          {{ article.title }}
-        </Component>
-        <p property="description" class="lead">
-          {{ article.description }}
-        </p>
-      </div>
-    </article>
+    <div class="inner">
+      <ul>
+        <li v-for="tag in article.tags" :key="tag" property="keywords" class="h3 tag">
+          {{ tag }}
+        </li>
+      </ul>
+      <Component :is="titleTag" property="headline">
+        {{ article.title }}
+      </Component>
+      <p property="description" class="lead">
+        {{ article.description }}
+      </p>
+    </div>
   </NuxtLink>
 </template>
--- components/work/Card.vue
+++ components/work/Card.vue
@@ -12,44 +12,38 @@ const localePath = useLocalePath()
 </script>
 
 <template>
-  <NuxtLink :to="localePath(`${work._path}`)" class="card">
-    <article vocab="https://schema.org/" typeof="Article">
-      <span property="mainEntityOfPage" typeof="WebPage">
-        <span property="id" :content="`${baseUrl}${localePath(`${work._path}`)}`" />
-      </span>
-      <span property="author publisher" typeof="Organization">
-        <span property="name" :content="t('name')" />
-        <span property="url" :content="baseUrl" />
-      </span>
-      <time property="dateCreated datePublished" :datetime="work.createdAt.toString()" />
-      <time property="dateModified" :datetime="work.updatedAt.toString()" />
-      <span v-for="tag in work.tags" :key="tag" property="keywords" :content="tag" />
-      <span property="articleSection" :content="t('works.title')" />
-      <span property="description" :content="work.description" />
+  <NuxtLink :to="localePath(`${work._path}`)" class="card" vocab="https://schema.org/" typeof="Article">
+    <span property="author publisher" typeof="Organization">
+      <span property="name" :content="t('name')" />
+      <span property="url" :content="baseUrl" />
+    </span>
+    <time property="dateCreated datePublished" :datetime="work.createdAt.toString()" />
+    <time property="dateModified" :datetime="work.updatedAt.toString()" />
+    <span v-for="tag in work.tags" :key="tag" property="keywords" :content="tag" />
+    <span property="articleSection" :content="t('works.title')" />
+    <span property="description" :content="work.description" />
 
-      <AppPicture
-        :picture="work"
-        type="thumbnail"
-        sizes="342px xs:348px md:460px xl:584px"
-        :img-attrs="{ property: 'image' }"
+    <AppPicture
+      :picture="work"
+      type="thumbnail"
+      sizes="342px xs:348px md:460px xl:584px"
+      :img-attrs="{ property: 'image' }"
+    />
+
+    <div class="overlay" />
+
+    <div class="inner work">
+      <span class="h3">
+        {{ work.category }}
+      </span>
+      <Component :is="titleTag" property="headline" class="work">
+        {{ work.title }}
+      </Component>
+      <ContentRendererMarkdown
+        :value="work"
+        property="articleBody"
+        class="lead"
       />
-
-      <div class="overlay" />
-
-      <div class="inner work">
-        <span class="h3">
-          {{ work.category }}
-        </span>
-        <Component :is="titleTag" property="headline" class="work">
-          {{ work.title }}
-        </Component>
-        <ContentRendererMarkdown
-          :value="work"
-          tag="p"
-          property="articleBody"
-          class="lead"
-        />
-      </div>
-    </article>
+    </div>
   </NuxtLink>
 </template>
