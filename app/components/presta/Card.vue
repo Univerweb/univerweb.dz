5fdedfa8a2e0ed9ed0c3279d66b9f86e8570ad7f
Houssem Nedjar <h.nedjar@univerweb.dz>
2025-02-23 14:33:18 +0100

    feat: ajouter un composant Card pour encapsuler la logique d'affichage des prestations dans CardGroup

---
+++ app/components/presta/Card.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import type { Presta } from '@/types'
+
+defineProps<{
+  presta: Presta
+  titleTag: string
+}>()
+</script>
+
+<template>
+  <div class="card-presta">
+    <ul>
+      <li v-for="tag in presta.tags" :key="tag" property="keywords" class="tag">
+        {{ tag }}
+      </li>
+    </ul>
+
+    <Component :is="titleTag">
+      {{ presta.title }}
+    </Component>
+
+    <p class="lead">
+      {{ presta.description }}
+    </p>
+
+    <AppMore :path="presta._path" :label="presta.title" />
+  </div>
+</template>
