253eac3e0a452ff2cc2675978df1dd531644e5db
Houssem Nedjar <h.nedjar@univerweb.dz>
2025-02-23 13:40:00 +0100

    feat: ajouter un composant CardGroup pour afficher les prestations avec un style amélioré

--- app/pages/prestations/index.vue
+++ app/pages/prestations/index.vue
@@ -1,49 +1,11 @@
 <script setup lang="ts">
-import type { Presta } from '../../types'
-
-const { path } = useRoute()
-const localePath = useLocalePath()
-const { t } = useI18n()
-
-const { data: prestations } = await useAsyncData(
-  `prestations${path}`,
-  () => queryContent<Pick<Presta, '_path' | 'title' | 'description' | 'tags' | 'body'>>(localePath('prestations'))
-    .only(['_path', 'title', 'description', 'tags', 'body'])
-    .find(),
-  { watch: [localePath] },
-)
-
-useSeoMeta(useMeta('presta'))
 useHead({ script: [useBreadcrumb('presta')] })
+useSeoMeta(useMeta('presta'))
 </script>
 
 <template>
   <main>
-    <div class="container intro">
-      <h1>{{ t('presta.headline') }}</h1>
-    </div>
-
-    <div v-for="(prestation, index) in prestations" :key="index" class="container">
-      <div :class="`intro ${['intro-end', '', 'intro-end'][index]}`">
-        <h2>
-          {{ prestation.title }}
-        </h2>
-      </div>
-      <div class="row row-center">
-        <p :class="`lead col col--${['7-12', '1-6', '7-12'][index]}`">
-          {{ prestation.description }}
-        </p>
-        <ul :class="`col col--${['3-6', '7-10', '3-6'][index]}`">
-          <li v-for="tag in prestation.tags" :key="tag" class="tag tag-bold">
-            {{ tag }}
-          </li>
-        </ul>
-      </div>
-      <div class="row">
-        <AppMore :path="prestation._path" :label="prestation.title" :class="`start col--${['7-12', '1-6', '7-12'][index]}`" />
-      </div>
-    </div>
-
+    <PrestaCardGroup lead tags />
     <LazyAppRequest />
   </main>
 </template>
