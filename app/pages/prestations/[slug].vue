113122c8f6ab8380ed633a0a00d60f9b40ff6e17
Houssem Nedjar <h.nedjar@univerweb.dz>
2025-02-26 15:16:09 +0100

    feat: mettre à jour la structure des prestations pour intégrer les propriétés 'lead', 'intro', 'tags' et améliorer la présentation des sections FAQ et processus

--- app/pages/prestations/[slug].vue
+++ app/pages/prestations/[slug].vue
@@ -9,7 +9,7 @@ const { path } = useRoute()
 const { data: presta } = await useAsyncData(
   `presta${path}`,
   () => queryContent<Presta>()
-    .only(['title', 'description', 'features', 'process', 'faq'])
+    .only(['title', 'description', 'lead', 'intro', 'features', 'tags', 'process', 'faq'])
     .where({ _path: path })
     .findOne(),
 )
@@ -89,68 +89,102 @@ useSeoMeta({
 </script>
 
 <template>
-  <main v-if="presta">
+  <main v-if="presta" class="presta">
     <section vocab="https://schema.org/" typeof="Service">
       <div class="container intro">
         <AppBack :path="localePath('prestations')" :label="t('menu[1].label')" />
-        <h1 property="name serviceType">
+        <h1 property="name">
           {{ presta.title }}
         </h1>
-        <p class="lead" property="description">
-          {{ presta.description }}
+        <h2 property="description">
+          {{ presta.lead }}
+        </h2>
+        <p class="lead">
+          {{ presta.intro }}
         </p>
       </div>
 
-      <div class="container">
-        <h2>{{ t('presta.included') }}</h2>
+      <div class="container features">
         <div class="row">
-          <div v-for="feature in presta.features" :key="feature.title" class="col col--1-9">
-            <h3>{{ feature.title }}</h3>
-            <p>{{ feature.description }}</p>
+          <h2 class="col col--1-4">
+            {{ presta.features.title }}
+          </h2>
+
+          <div class="col col--5-13">
+            <template v-for="feature in presta.features.list" :key="feature.title">
+              <h3 property="serviceType">
+                {{ feature.title }}
+              </h3>
+              <p>{{ feature.description }}</p>
+            </template>
           </div>
         </div>
       </div>
 
-      <div v-if="presta.process" class="container">
-        <h2>{{ t('presta.process') }}</h2>
-        <ol class="row">
-          <li v-for="(process, index) in presta.process" :key="process.title" :class="`big-count col ${['col--1-6 col--1-3', 'col--7-12 col--4-6', 'col--1-6 col--7-9', 'col--7-12 col--10-12'][index % 4]}`">
-            <h3>{{ process.title }}</h3>
-            <p>{{ process.description }}</p>
-          </li>
-        </ol>
-      </div>
-    </section>
+      <div class="container tags-group">
+        <div class="row">
+          <h2 class="col col--1-6">
+            {{ presta.tags.title }}
+          </h2>
 
-    <section v-if="presta.faq" class="container" vocab="https://schema.org/" typeof="FAQPage">
-      <h2>{{ t('presta.faq') }}</h2>
-      <div v-for="(faq, index) in presta.faq" :key="index" class="faq" property="mainEntity" typeof="Question">
-        <h3 class="col">
-          <button :id="`faq-header-${index + 1}`" :aria-expanded="activeIndex === index" :aria-controls="`faq-panel-${index + 1}`" @click="toggle(index)">
-            <span property="name">
-              {{ faq.question }}
-            </span>
-            <span class="icon" aria-hidden="true">
-              <span :class="{ expanded: activeIndex === index }" />
-              <span />
-            </span>
-          </button>
-        </h3>
-        <Transition @before-enter="beforeEnter" @enter="enter" @after-enter="afterEnter" @before-leave="beforeLeave" @leave="leave">
-          <section
-            v-show="activeIndex === index"
-            :id="`faq-panel-${index + 1}`"
-            :aria-labelledby="`faq-header-${index + 1}`"
-            :aria-hidden="activeIndex !== index"
-            class="row"
-            property="acceptedAnswer"
-            typeof="Answer"
-          >
-            <p class="col col--1-9" property="text">
-              {{ faq.answer }}
-            </p>
-          </section>
-        </Transition>
+          <ul class="col col--7-12 tags">
+            <li v-for="tag in presta.tags.list" :key="tag">
+              {{ tag }}
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="container">
+        <div class="row">
+          <h2 col class="col col--1-4">
+            {{ presta.process.title }}
+          </h2>
+
+          <ol class="col row">
+            <li v-for="(process, index) in presta.process.steps" :key="process.title" :class="`big-count col ${['col--1-6 col--1-3', 'col--7-12 col--4-6', 'col--1-6 col--7-9', 'col--7-12 col--10-12'][index % 4]}`">
+              <h3>{{ process.title }}</h3>
+              <p>{{ process.description }}</p>
+            </li>
+          </ol>
+        </div>
+      </div>
+
+      <div class="container faq" vocab="https://schema.org/" typeof="FAQPage">
+        <div class="row">
+          <h2 col class="col col--1-4">
+            {{ presta.faq.title }}
+          </h2>
+
+          <div v-for="(faq, index) in presta.faq.questions" :key="index" class="col question" property="mainEntity" typeof="Question">
+            <h3>
+              <button :id="`faq-header-${index + 1}`" :aria-expanded="activeIndex === index" :aria-controls="`faq-panel-${index + 1}`" @click="toggle(index)">
+                <span property="name">
+                  {{ faq.question }}
+                </span>
+                <span class="icon" aria-hidden="true">
+                  <span :class="{ expanded: activeIndex === index }" />
+                  <span />
+                </span>
+              </button>
+            </h3>
+            <Transition @before-enter="beforeEnter" @enter="enter" @after-enter="afterEnter" @before-leave="beforeLeave" @leave="leave">
+              <section
+                v-show="activeIndex === index"
+                :id="`faq-panel-${index + 1}`"
+                :aria-labelledby="`faq-header-${index + 1}`"
+                :aria-hidden="activeIndex !== index"
+                class="row"
+                property="acceptedAnswer"
+                typeof="Answer"
+              >
+                <p class="col col--1-9" property="text">
+                  {{ faq.answer }}
+                </p>
+              </section>
+            </Transition>
+          </div>
+        </div>
       </div>
     </section>
 
