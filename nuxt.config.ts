b397de3f88e5f29094bb4235d5004c73d40cba77
Houssem Nedjar <h.nedjar@univerweb.dz>
2025-04-24 09:47:47 +0100

    fix: corriger le chemin d'importation du fichier de variables SCSS

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -48,7 +48,7 @@ export default defineNuxtConfig({
     css: {
       preprocessorOptions: {
         scss: {
-          additionalData: '@use "@/assets/scss/_variable.scss" as *; @use "@/assets/scss/_mixins.scss" as *;',
+          additionalData: '@use "@/assets/scss/_variables.scss" as *; @use "@/assets/scss/_mixins.scss" as *;',
         },
       },
     },
