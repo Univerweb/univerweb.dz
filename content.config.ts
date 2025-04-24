249935327225168191128d6dac334043ee9036a6
Houssem Nedjar <h.nedjar@univerweb.dz>
2025-04-24 18:44:37 +0100

    feat: ajouter la configuration des collections pour les prestations

--- content.config.ts
+++ content.config.ts
@@ -12,6 +12,36 @@ const work = commonSchema.extend({
   link: z.string().optional(),
 })
 
+const presta = commonSchema.extend({
+  cta: z.string(),
+  intro: z.array(z.string()),
+  solutions: z.object({
+    title: z.string(),
+    list: z.array(z.object({
+      title: z.string(),
+      description: z.string(),
+    })),
+  }),
+  features: z.object({
+    title: z.string(),
+    tags: z.array(z.string()),
+  }),
+  process: z.object({
+    title: z.string(),
+    steps: z.array(z.object({
+      title: z.string(),
+      description: z.string(),
+    })),
+  }),
+  faq: z.object({
+    title: z.string(),
+    questions: z.array(z.object({
+      question: z.string(),
+      answer: z.string(),
+    })),
+  }),
+})
+
 const post = commonSchema.extend({
   author: z.object({
     name: z.string(),
@@ -38,6 +68,24 @@ export default defineContentConfig({
       source: 'ar/realisations/*.yaml',
       type: 'page',
       schema: work,
+    }),
+    presta_fr: defineCollection({
+      source: {
+        include: 'fr/prestations/*.yaml',
+        prefix: 'prestations',
+      },
+      type: 'page',
+      schema: presta,
+    }),
+    presta_en: defineCollection({
+      source: 'en/prestations/*.yaml',
+      type: 'page',
+      schema: presta,
+    }),
+    presta_ar: defineCollection({
+      source: 'ar/prestations/*.yaml',
+      type: 'page',
+      schema: presta,
     }),
     post_fr: defineCollection({
       source: {
