998e1582f2a97d929f78fd54cd91296f770fc7d3
Houssem Nedjar <h.nedjar@univerweb.dz>
2025-04-24 18:48:15 +0100

    feat: ajouter les composants Card et CardGroup pour les prestations

---
+++ app/components/presta/Card.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import type { Collections } from '@nuxt/content'
+
+defineProps<{
+  presta: Pick<Collections['presta_fr' | 'presta_en' | 'presta_ar'], 'path' | 'title' | 'cta' | 'lead' | 'solutions'>
+  titleTag: string
+}>()
+</script>
+
+<template>
+  <div class="card-presta" vocab="https://schema.org/" typeof="Service">
+    <ul class="tags">
+      <li v-for="solution in presta.solutions.list.slice(0, 3)" :key="solution.title" property="serviceType">
+        {{ solution.title }}
+      </li>
+    </ul>
+
+    <Component :is="titleTag" property="name">
+      <NuxtLink :to="presta.path">
+        {{ presta.title }}
+      </NuxtLink>
+    </Component>
+
+    <p class="lead" property="description">
+      {{ presta.lead }}
+    </p>
+
+    <AppMore :path="presta.path" :label="presta.cta" />
+  </div>
+</template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,6 +2,7 @@
   <main>
     <AppWelcome />
     <LazyWorkCardGroup headline-tag="h2" title-tag="h3" :limit="6" more />
+    <LazyPrestaCardGroup headline-tag="h2" title-tag="h3" />
     <LazyPostCardGroup headline-tag="h2" title-tag="h3" :limit="2" more />
     <LazyAppRequest class-name="h1" />
   </main>
