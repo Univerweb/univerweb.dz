9d86e3adfbdf04ade762f0db724a738f23c6acdb
Houssem Nedjar <h.nedjar@univerweb.dz>
2025-04-29 15:10:55 +0100

    fix: réorganiser la structure des sections pour une meilleure présentation des méthodes et choix d'agence

--- app/pages/agence.vue
+++ app/pages/agence.vue
@@ -20,33 +20,33 @@ useSeoMeta(useMeta('agency'))
     </div>
 
     <div class="container">
-      <div class="intro">
-        <h2 class="h1">
+      <div class="row">
+        <h2 class="col col--1-5">
           {{ t('agency.method.title') }}
         </h2>
+        <ol class="col row">
+          <li v-for="(value, name, index) in (tm('agency.method.content') as Agency)" :key="index" :class="`big-count col ${['col--1-6 col--1-3', 'col--7-12 col--4-6', 'col--1-6 col--7-9', 'col--7-12 col--10-12'][index]}`">
+            <h3>{{ name }}</h3>
+            <p class="lead">
+              {{ rt(value) }}
+            </p>
+          </li>
+        </ol>
       </div>
-      <ol class="row">
-        <li v-for="(value, name, index) in (tm('agency.method.content') as Agency)" :key="index" :class="`big-count col ${['col--1-6 col--1-3', 'col--7-12 col--4-6', 'col--1-6 col--7-9', 'col--7-12 col--10-12'][index]}`">
-          <h3>{{ name }}</h3>
-          <p class="lead">
-            {{ rt(value) }}
-          </p>
-        </li>
-      </ol>
     </div>
 
     <div class="container">
-      <div class="intro">
-        <h2 class="h1">
+      <div class="row">
+        <h2 class="col col--1-5">
           {{ t('agency.choose.title') }}
         </h2>
-      </div>
-      <div class="row">
-        <div v-for="(value, name, index) in (tm('agency.choose.content') as Agency)" :key="index" :class="`col ${['col--1-6', 'col--7-12', 'col--1-6', 'col--7-12'][index]}`">
-          <h3>{{ name }}</h3>
-          <p class="lead">
-            {{ rt(value) }}
-          </p>
+        <div class="col row">
+          <div v-for="(value, name, index) in (tm('agency.choose.content') as Agency)" :key="index" :class="`col ${['col--1-6', 'col--7-12', 'col--1-6', 'col--7-12'][index]}`">
+            <h3>{{ name }}</h3>
+            <p class="lead">
+              {{ rt(value) }}
+            </p>
+          </div>
         </div>
       </div>
     </div>
