f88aa27463c99a97a109c6325b54e1db4b931c33
Houssem Nedjar <h.nedjar@univerweb.dz>
2025-05-02 16:54:45 +0100

    refactor: simplifier la structure des conteneurs dans les pages agence et prestations

--- app/pages/agence.vue
+++ app/pages/agence.vue
@@ -18,34 +18,30 @@ useSeoPage('agence')
       </p>
     </div>
 
-    <div class="container">
-      <div class="row">
-        <h2 class="col col--1-5">
-          {{ t('agence.method.title') }}
-        </h2>
-        <ol class="col row">
-          <li v-for="(value, name, index) in (tm('agence.method.content') as Agence)" :key="index" :class="`big-count col ${['col--1-6 col--1-3', 'col--7-12 col--4-6', 'col--1-6 col--7-9', 'col--7-12 col--10-12'][index]}`">
-            <h3>{{ name }}</h3>
-            <p class="lead">
-              {{ rt(value) }}
-            </p>
-          </li>
-        </ol>
-      </div>
+    <div class="container row">
+      <h2 class="col col--1-5">
+        {{ t('agence.method.title') }}
+      </h2>
+      <ol class="col row">
+        <li v-for="(value, name, index) in (tm('agence.method.content') as Agence)" :key="index" :class="`big-count col ${['col--1-6 col--1-3', 'col--7-12 col--4-6', 'col--1-6 col--7-9', 'col--7-12 col--10-12'][index]}`">
+          <h3>{{ name }}</h3>
+          <p class="lead">
+            {{ rt(value) }}
+          </p>
+        </li>
+      </ol>
     </div>
 
-    <div class="container">
-      <div class="row">
-        <h2 class="col col--1-5">
-          {{ t('agence.choose.title') }}
-        </h2>
-        <div class="col row">
-          <div v-for="(value, name, index) in (tm('agence.choose.content') as Agence)" :key="index" :class="`col ${['col--1-6', 'col--7-12', 'col--1-6', 'col--7-12'][index]}`">
-            <h3>{{ name }}</h3>
-            <p class="lead">
-              {{ rt(value) }}
-            </p>
-          </div>
+    <div class="container row">
+      <h2 class="col col--1-5">
+        {{ t('agence.choose.title') }}
+      </h2>
+      <div class="col row">
+        <div v-for="(value, name, index) in (tm('agence.choose.content') as Agence)" :key="index" :class="`col ${['col--1-6', 'col--7-12', 'col--1-6', 'col--7-12'][index]}`">
+          <h3>{{ name }}</h3>
+          <p class="lead">
+            {{ rt(value) }}
+          </p>
         </div>
       </div>
     </div>
--- app/pages/prestations/[slug].vue
+++ app/pages/prestations/[slug].vue
@@ -81,98 +81,84 @@ useSeoSlug({
         class="banner"
       />
 
-      <div class="container">
-        <div class="row">
-          <h2 class="col" property="description">
-            {{ presta.lead }}
-          </h2>
-          <div class="col col--1-9 lead">
-            <p v-for="(paragraph, index) in presta.intro" :key="index">
-              {{ paragraph }}
-            </p>
-          </div>
+      <div class="container row">
+        <h2 class="col" property="description">
+          {{ presta.lead }}
+        </h2>
+        <div class="col col--1-9 lead">
+          <p v-for="(paragraph, index) in presta.intro" :key="index">
+            {{ paragraph }}
+          </p>
         </div>
       </div>
 
-      <div class="container features">
-        <div class="row">
-          <h2 class="col col--1-5">
-            {{ presta.solutions.title }}
-          </h2>
-
-          <div class="col col--6-13">
-            <template v-for="solution in presta.solutions.list" :key="solution.title">
-              <h3>{{ solution.title }}</h3>
-              <p>{{ solution.description }}</p>
-            </template>
-          </div>
+      <div class="container row features">
+        <h2 class="col col--1-5">
+          {{ presta.solutions.title }}
+        </h2>
+        <div class="col col--6-13">
+          <template v-for="solution in presta.solutions.list" :key="solution.title">
+            <h3>{{ solution.title }}</h3>
+            <p>{{ solution.description }}</p>
+          </template>
         </div>
       </div>
 
-      <div class="container tags-group">
-        <div class="row">
-          <h2 class="col col--1-6">
-            {{ presta.features.title }}
-          </h2>
-
-          <ul class="col col--7-12 tags">
-            <li v-for="tag in presta.features.tags" :key="tag">
-              {{ tag }}
-            </li>
-          </ul>
-        </div>
+      <div class="container row tags-group">
+        <h2 class="col col--1-6">
+          {{ presta.features.title }}
+        </h2>
+        <ul class="col col--7-12 tags">
+          <li v-for="tag in presta.features.tags" :key="tag">
+            {{ tag }}
+          </li>
+        </ul>
       </div>
 
-      <div class="container">
-        <div class="row">
-          <h2 col class="col col--1-5">
-            {{ presta.process.title }}
-          </h2>
-
-          <ol class="col row">
-            <li v-for="(process, index) in presta.process.steps" :key="process.title" :class="`big-count col ${['col--1-6 col--1-3', 'col--7-12 col--4-6', 'col--1-6 col--7-9', 'col--7-12 col--10-12'][index % 4]}`">
-              <h3>{{ process.title }}</h3>
-              <p>{{ process.description }}</p>
-            </li>
-          </ol>
-        </div>
+      <div class="container row">
+        <h2 col class="col col--1-5">
+          {{ presta.process.title }}
+        </h2>
+        <ol class="col row">
+          <li v-for="(process, index) in presta.process.steps" :key="process.title" :class="`big-count col ${['col--1-6 col--1-3', 'col--7-12 col--4-6', 'col--1-6 col--7-9', 'col--7-12 col--10-12'][index % 4]}`">
+            <h3>{{ process.title }}</h3>
+            <p>{{ process.description }}</p>
+          </li>
+        </ol>
       </div>
 
-      <div class="container faq" vocab="https://schema.org/" typeof="FAQPage">
-        <div class="row">
-          <h2 col class="col col--1-5">
-            {{ presta.faq.title }}
-          </h2>
-
-          <div class="col">
-            <div v-for="(faq, index) in presta.faq.questions" :key="index" class="question" property="mainEntity" typeof="Question">
-              <h3>
-                <button :id="`faq-header-${index + 1}`" :aria-expanded="activeIndex === index" :aria-controls="`faq-panel-${index + 1}`" @click="toggle(index)">
-                  <span property="name">
-                    {{ faq.question }}
-                  </span>
-                  <span class="icon" aria-hidden="true">
-                    <span :class="{ expanded: activeIndex === index }" />
-                    <span />
-                  </span>
-                </button>
-              </h3>
-              <Transition @before-enter="beforeEnter" @enter="enter" @after-enter="afterEnter" @before-leave="beforeLeave" @leave="leave">
-                <section
-                  v-show="activeIndex === index"
-                  :id="`faq-panel-${index + 1}`"
-                  :aria-labelledby="`faq-header-${index + 1}`"
-                  :aria-hidden="activeIndex !== index"
-                  class="row"
-                  property="acceptedAnswer"
-                  typeof="Answer"
-                >
-                  <p class="col col--1-9" property="text">
-                    {{ faq.answer }}
-                  </p>
-                </section>
-              </Transition>
-            </div>
+      <div class="container row faq" vocab="https://schema.org/" typeof="FAQPage">
+        <h2 col class="col col--1-5">
+          {{ presta.faq.title }}
+        </h2>
+        <div class="col">
+          <div v-for="(faq, index) in presta.faq.questions" :key="index" class="question" property="mainEntity" typeof="Question">
+            <h3>
+              <button :id="`faq-header-${index + 1}`" :aria-expanded="activeIndex === index" :aria-controls="`faq-panel-${index + 1}`" @click="toggle(index)">
+                <span property="name">
+                  {{ faq.question }}
+                </span>
+                <span class="icon" aria-hidden="true">
+                  <span :class="{ expanded: activeIndex === index }" />
+                  <span />
+                </span>
+              </button>
+            </h3>
+            <Transition @before-enter="beforeEnter" @enter="enter" @after-enter="afterEnter" @before-leave="beforeLeave" @leave="leave">
+              <section
+                v-show="activeIndex === index"
+                :id="`faq-panel-${index + 1}`"
+                :aria-labelledby="`faq-header-${index + 1}`"
+                :aria-hidden="activeIndex !== index"
+                class="row"
+                property="acceptedAnswer"
+                typeof="Answer"
+              >
+                <p class="col col--1-9" property="text">
+                  {{ faq.answer }}
+                </p>
+              </section>
+            </Transition>
           </div>
         </div>
       </div>
