5df5c1f6595acef580c607b637ce4c51193b42ec
Houssem Nedjar <h.nedjar@univerweb.dz>
2025-05-05 15:22:48 +0100

    fix: refactor le composant de navigation pour améliorer la structure et mettre à jour les traductions

--- app/components/app/Nav.vue
+++ app/components/app/Nav.vue
@@ -13,33 +13,11 @@ const { t } = useI18n()
 </script>
 
 <template>
-  <div class="container post-nav">
+  <div class="container slug-nav">
     <NuxtLink v-if="prev" :to="prev.path" class="link prev" :data-text="t('navigation.previous')">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="6"
-        height="10"
-        viewBox="0 0 6 10"
-        fill="currentColor"
-        role="img"
-        aria-hidden="true"
-      >
-        <path d="M6 8.7 2.2 4.9 6 1.1 4.9 0 1.1 3.8 0 4.9 1.1 6l3.8 3.8L6 8.7Z" />
-      </svg>
       {{ prev.title }}
     </NuxtLink>
     <NuxtLink v-if="next" :to="next.path" class="link next" :data-text="t('navigation.next')">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="6"
-        height="10"
-        viewBox="0 0 6 10"
-        fill="currentColor"
-        role="img"
-        aria-hidden="true"
-      >
-        <path d="m0 1.1 3.8 3.8L0 8.7l1.1 1.1L4.9 6 6 4.9 4.9 3.8 1.1 0 0 1.1Z" />
-      </svg>
       {{ next.title }}
     </NuxtLink>
   </div>
