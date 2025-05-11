e62146c5d7b16590affee8d8f5ff5051146f8708
Houssem Nedjar <h.nedjar@univerweb.dz>
2025-05-11 11:48:45 +0100

    fix: améliorer l'accessibilité et la sémantique des composants en ajoutant des attributs ARIA et en réorganisant le code

--- app/pages/blog/[slug].vue
+++ app/pages/blog/[slug].vue
@@ -36,38 +36,54 @@ useSeoSlug({
   <main v-if="post" class="blog">
     <article vocab="https://schema.org/" typeof="Article" aria-labelledby="title">
       <header class="container intro">
+        <meta property="description" :content="post.description">
+        <meta property="articleSection" :content="t('navigation.menu[4].label')">
+        <meta property="dateModified" :content="new Date(post.updatedAt).toISOString()">
+        <span property="mainEntityOfPage" typeof="WebPage">
+          <link property="url" :href="baseUrl(path)">
+        </span>
+        <span property="publisher" typeof="Organization">
+          <span property="name" :content="t('site.name')" />
+          <link property="url" :href="localeBaseUrl">
+        </span>
+
         <AppBack path="blog" :label="t('navigation.menu[4].label')" />
         <h1 id="title" property="headline">
           {{ post.title }}
         </h1>
-        <span property="description" :content="post.description" />
-        <span property="articleSection" :content="t('navigation.menu[4].label')" />
-        <span property="mainEntityOfPage" typeof="WebPage">
-          <span property="id" :content="baseUrl(path)" />
-        </span>
-        <span property="publisher" typeof="Organization">
-          <span property="name" :content="t('site.name')" />
-          <span property="url" :content="localeBaseUrl" />
-        </span>
-        <div class="meta">
-          <NuxtTime property="dateCreated datePublished" :datetime="post.createdAt" :locale="head.htmlAttrs.lang" date-style="long" />
-          <time property="dateModified" :datetime="new Date(post.updatedAt).toISOString()" />
-          {{ t('blog.by') }}
-          <span v-if="post.author && post.author.name && post.author.url" property="author" typeof="Person" class="author">
-            <a :href="post.author.url" rel="author" target="_blank" property="url" :content="post.author.url">
+
+        <dl class="meta">
+          <dt id="published" class="visually-hidden">
+            {{ t('blog.ariaLabelledby.published') }}
+          </dt>
+          <dd aria-labelledby="published">
+            <NuxtTime property="dateCreated datePublished" :datetime="post.createdAt" :locale="head.htmlAttrs.lang" date-style="long" />
+          </dd>
+          <dt id="author" class="visually-hidden">
+            {{ t('blog.ariaLabelledby.author') }}
+          </dt>
+          <dd aria-labelledby="author">
+            {{ t('blog.by') }}
+            <a v-if="post.author && post.author.name && post.author.url" :href="post.author.url" target="_blank" property="author" typeof="Person">
               <span property="name">@{{ post.author.name }}</span>
+              <link property="url" :href="post.author.url">
             </a>
-          </span>
-          <span v-else property="author" typeof="Organization" class="author">
-            <span property="name">@{{ t('site.name') }}</span>
-            <span property="url" :content="localeBaseUrl" />
-          </span>
-          <ul class="tags">
-            <li v-for="tag in post.tags" :key="tag" property="keywords">
-              {{ tag }}
-            </li>
-          </ul>
-        </div>
+            <span v-else property="author" typeof="Organization">
+              <span property="name">@{{ t('site.name') }}</span>
+              <link property="url" :href="localeBaseUrl">
+            </span>
+          </dd>
+          <dt id="keywords" class="visually-hidden">
+            {{ t('blog.ariaLabelledby.keywords') }}
+          </dt>
+          <dd aria-labelledby="keywords">
+            <ul class="tags">
+              <li v-for="tag in post.tags" :key="tag" property="keywords">
+                {{ tag }}
+              </li>
+            </ul>
+          </dd>
+        </dl>
       </header>
 
       <AppPicture
@@ -78,11 +94,11 @@ useSeoSlug({
       />
 
       <section class="container row row-no-gap">
-        <ContentRenderer :value="post" class="col col--2-12 col--3-11 content" />
+        <ContentRenderer :value="post" property="articleBody" class="col col--2-12 col--3-11 content" />
       </section>
-
-      <LazyPostShare :title="post.title" :path="baseUrl(path)" />
     </article>
+
+    <LazyPostShare :title="post.title" :path="baseUrl(path)" />
 
     <LazyAppNav :prev="postSurround?.[0]" :next="postSurround?.[1]" :aria-label="t('blog.ariaLabels.nav')" />
   </main>
