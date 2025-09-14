04d034716b3ffbfecc8c839621f7c18a4e1311d7
Houssem Nedjar <h.nedjar@univerweb.dz>
2025-09-14 19:33:49 +0100

    fix: NuxtLoadingIndicator backgroundSize

--- app/app.vue
+++ app/app.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 const head = useLocaleHead()
 const { y } = useWindowScroll()
-const { progress } = useLoadingIndicator()
 const { finalizePendingLocaleChange } = useI18n()
 
 useHead({
@@ -18,10 +17,7 @@ const onBeforeEnter = async () => {
 </script>
 
 <template>
-  <NuxtLoadingIndicator
-    color="repeating-linear-gradient(to right, #50c8f0 0%, #28285a 50%, #50c8f0 100%)"
-    :style="{ backgroundSize: progress === 0 ? 'auto' : `${(100 / progress) * 100}% auto`, transform: `scaleX(${progress / 100})` }"
-  />
+  <NuxtLoadingIndicator color="repeating-linear-gradient(to right, #50c8f0 0%, #28285a 50%, #50c8f0 100%)" />
   <NuxtLayout>
     <AppHeader />
     <NuxtPage id="main" :transition="{ name: 'page', mode: 'out-in', onBeforeEnter }" />
