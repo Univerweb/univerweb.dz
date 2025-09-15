cfad29802d158d551cb5db69f8dfc468f4e50cba
Houssem Nedjar <h.nedjar@univerweb.dz>
2025-09-15 23:16:29 +0100

    fix: AppIcon

--- app/components/app/Icon.vue
+++ app/components/app/Icon.vue
@@ -9,8 +9,8 @@ defineProps<{
 <template>
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    :width
-    :height
+    :width="width || 24"
+    :height="height || 24"
     :viewBox="`0 0 ${width || 24} ${height || 24}`"
     :fill="fill || 'currentColor'"
     role="img"
