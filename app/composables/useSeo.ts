0f5a478b71fc914e7f7f9cd23cb911f03c6014b8
Houssem Nedjar <h.nedjar@univerweb.dz>
2025-09-17 01:04:37 +0100

    update: useSeo

--- app/composables/useSeo.ts
+++ app/composables/useSeo.ts
@@ -1,15 +1,15 @@
-interface SeoOptions {
-  page?: 'home' | 'realisations' | 'prestations' | 'agence' | 'contact' | 'blog'
-  pageSlug?: 'realisations' | 'prestations' | 'blog'
-  title?: () => string
-  description?: () => string
+interface Options {
+  page: 'home' | 'realisations' | 'prestations' | 'agence' | 'contact' | 'blog'
+  pageSlug?: boolean
+  title: () => string
+  description: () => string
   ogTitle?: () => string
   ogImageAlt?: () => string
   ogImageWidth?: number
   ogImageHeight?: number
 }
 
-export function useSeo(options: SeoOptions) {
+export function useSeo(options: Options) {
   const img = useImage()
   const localePath = useLocalePath()
   const { path } = useRoute()
@@ -18,9 +18,13 @@ export function useSeo(options: SeoOptions) {
   const mode = useColorMode({ disableTransition: false })
   const config = useRuntimeConfig()
 
-  const unrefString = (val: string | Ref<string>): string => {
-    return isRef(val) ? val.value : val
-  }
+  const { data: organization } = useAsyncData(
+    () => `seo-${locale.value}`,
+    () => queryCollection(`contact_${locale.value}`)
+      .select('address', 'platforms')
+      .first(),
+    { watch: [locale] },
+  )
 
   const locales = { fr: 'fr_FR', en: 'en_US', ar: 'ar_DZ' }
 
@@ -29,7 +33,7 @@ export function useSeo(options: SeoOptions) {
   const ogLocale = computed(() => locales[locale.value as keyof typeof locales] || 'fr_FR')
   const ogLocaleAlternate = computed(() => Object.values(locales).filter(localeCode => localeCode !== ogLocale.value))
 
-  const defaultOgImage = computed(() => baseUrl(`/images/univerweb${unrefString(locale) === 'ar' ? '-ar' : ''}.jpg`))
+  const defaultOgImage = computed(() => baseUrl(`/images/univerweb${locale.value === 'ar' ? '-ar' : ''}.jpg`))
   const defaultOgImageWidth = 2400
   const defaultOgImageHeight = 1256
 
@@ -37,58 +41,16 @@ export function useSeo(options: SeoOptions) {
   const linkAltEn = baseUrl(localePath(path, 'en'))
   const linkAltAr = baseUrl(localePath(path, 'ar'))
 
-  const title: () => string = (() => {
-    if (options.pageSlug && options.title && options.description) {
-      return options.title
-    }
-
-    else if (options.page) {
-      return () => t(`${options.page}.title`)
-    }
-
-    else {
-      return () => t('home.title')
-    }
-  })()
-
-  const description: () => string = (() => {
-    if (options.pageSlug && options.title && options.description) {
-      return options.description
-    }
-
-    else if (options.page) {
-      return () => t(`${options.page}.description`)
-    }
-
-    else {
-      return () => t('home.description')
-    }
-  })()
-
-  const ogTitle: () => string = (() => {
-    if (options.pageSlug && options.title && options.description) {
-      return options.ogTitle || options.title
-    }
-
-    else if (options.page) {
-      return title
-    }
-
-    else {
-      return () => t('home.title')
-    }
-  })()
+  const title: () => string = options.title
+  const description: () => string = options.description
+  const ogTitle: () => string = options.ogTitle || options.title || title
 
   const ogImage: ComputedRef<string | undefined> = (() => {
-    if (options.pageSlug && options.ogImageAlt) {
-      return computed(() =>
-        unrefString(
-          img(
-            unrefString(localePath(`${path}_banner`, 'fr')),
-            { format: 'webp', width: options.ogImageWidth || defaultOgImageWidth, height: options.ogImageHeight || defaultOgImageHeight },
-            { provider: 'cloudinary' },
-          ),
-        ),
+    if (options.ogImageAlt) {
+      return computed(() => img(localePath(`${path}_banner`, 'fr'),
+        { format: 'webp', width: options.ogImageWidth || defaultOgImageWidth, height: options.ogImageHeight || defaultOgImageHeight },
+        { provider: 'cloudinary' },
+      ),
       )
     }
 
@@ -97,20 +59,13 @@ export function useSeo(options: SeoOptions) {
     }
   })()
 
-  const ogImageAlt: () => string = (() => {
-    if (options.pageSlug && options.ogImageAlt) {
-      return options.ogImageAlt
-    }
-
-    else {
-      return () => t('site.logo')
-    }
-  })()
+  const ogImageAlt: () => string = options.ogImageAlt || (() => t('site.logo'))
 
   useSeoMeta({
     titleTemplate: computed(() => `%s | ${ogSiteName.value}`),
     title,
     description,
+    ogType: 'website',
     ogSiteName,
     ogTitle,
     ogDescription: description,
@@ -122,18 +77,28 @@ export function useSeo(options: SeoOptions) {
     ogImageAlt,
     ogImageWidth: options.ogImageWidth || defaultOgImageWidth,
     ogImageHeight: options.ogImageHeight || defaultOgImageHeight,
+    ogImageType: 'image/jpeg',
+    twitterCard: 'summary_large_image',
+    twitterSite: '@Univerweb',
     twitterTitle: ogTitle,
     twitterDescription: description,
     twitterImage: ogImage,
     twitterImageAlt: ogImageAlt,
     twitterImageWidth: options.ogImageWidth || defaultOgImageWidth,
     twitterImageHeight: options.ogImageHeight || defaultOgImageHeight,
+    twitterImageType: 'image/jpeg',
+    colorScheme: 'light dark',
     themeColor: computed(() => (mode.value === 'dark' ? '#111827' : '#fff')),
     appleMobileWebAppTitle: ogSiteName,
   })
 
   useHead({
     link: [
+      { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg', sizes: 'any' },
+      { rel: 'icon', type: 'image/png', href: '/favicon-48x48.png', sizes: '48x48' },
+      { rel: 'apple-touch-icon', href: '/apple-touch-icon.png', sizes: '180x180' },
+      { rel: 'manifest', href: '/site.webmanifest' },
+      { rel: 'canonical', href: ogUrl },
       { rel: 'alternate', href: linkAltFr, hreflang: 'x-default' },
       { rel: 'alternate', href: linkAltFr, hreflang: 'fr' },
       { rel: 'alternate', href: linkAltFr, hreflang: 'fr-FR' },
@@ -141,116 +106,65 @@ export function useSeo(options: SeoOptions) {
       { rel: 'alternate', href: linkAltEn, hreflang: 'en-US' },
       { rel: 'alternate', href: linkAltAr, hreflang: 'ar' },
       { rel: 'alternate', href: linkAltAr, hreflang: 'ar-DZ' },
-      { rel: 'canonical', href: ogUrl },
     ],
 
     script: [
       {
         type: 'application/ld+json',
         innerHTML: computed(() => {
-          const breadcrumbListElement: { '@type': 'ListItem', 'position': number, 'name': string, 'item'?: string }[] = [
-            {
-              '@type': 'ListItem',
-              'position': 1,
-              'name': ogSiteName.value,
-              'item': unrefString(localeBaseUrl),
-            },
+          const listItem = (position: number, name: string, item?: string) => ({
+            '@type': 'ListItem',
+            position,
+            name,
+            ...(item ? { item } : {}),
+          })
+
+          const items = [
+            listItem(1, ogSiteName.value, `${localeBaseUrl}`),
+            listItem(2, t(`navigation.menu.${options.page}`), baseUrl(localePath(options.page))),
+            ...(options.pageSlug ? [listItem(3, ogTitle(), ogUrl)] : []),
           ]
-
-          if (options.pageSlug) {
-            breadcrumbListElement.push(
-              {
-                '@type': 'ListItem',
-                'position': 2,
-                'name': t(`${options.pageSlug}.title`),
-                'item': unrefString(baseUrl(unrefString(localePath(options.pageSlug)))),
-              },
-              {
-                '@type': 'ListItem',
-                'position': 3,
-                'name': ogTitle(),
-              },
-            )
-          }
-
-          else {
-            breadcrumbListElement.push(
-              {
-                '@type': 'ListItem',
-                'position': 2,
-                'name': ogTitle(),
-              },
-            )
-          }
 
           return JSON.stringify({
             '@context': 'https://schema.org',
             '@type': 'BreadcrumbList',
-            'itemListElement': breadcrumbListElement,
+            'itemListElement': items,
           })
         }),
       },
 
       {
         type: 'application/ld+json',
-        innerHTML: computed(() => {
-          return JSON.stringify({
-            '@context': 'https://schema.org',
-            '@type': 'Organization',
-            'name': ogSiteName.value,
-            'url': unrefString(localeBaseUrl),
-            'image': {
-              '@type': 'ImageObject',
-              'url': defaultOgImage.value,
-              'width': `${defaultOgImageWidth}px`,
-              'height': `${defaultOgImageHeight}px`,
-            },
-            'logo': {
-              '@type': 'ImageObject',
-              'url': unrefString(baseUrl('/favicon.svg')),
-              'width': '256px',
-              'height': '256px',
-            },
-            'email': unrefString(config.public.baseEmail),
-            'telephone': unrefString(config.public.baseMobile),
-            'sameAs': [
-              'https://x.com/Univerweb',
-              'https://www.facebook.com/Univerweb',
-              'https://www.linkedin.com/company/Univerweb',
-              'https://github.com/Univerweb',
-            ],
-            'address': {
-              '@type': 'PostalAddress',
-              'streetAddress': t('site.address.street'),
-              'postalCode': t('site.address.postalCode'),
-              'addressLocality': t('site.address.locality'),
-              'addressRegion': t('site.address.locality'),
-              'addressCountry': 'DZ',
-            },
-          })
-        }),
+        innerHTML: {
+          '@context': 'https://schema.org',
+          '@type': 'Organization',
+          'name': ogSiteName.value,
+          'url': localeBaseUrl,
+          'image': {
+            '@type': 'ImageObject',
+            'url': defaultOgImage.value,
+            'width': `${defaultOgImageWidth}px`,
+            'height': `${defaultOgImageHeight}px`,
+          },
+          'logo': {
+            '@type': 'ImageObject',
+            'url': baseUrl('/favicon.svg'),
+            'width': '256px',
+            'height': '256px',
+          },
+          'email': config.public.baseEmail,
+          'telephone': config.public.baseMobile,
+          'sameAs': () => organization.value!.platforms?.map(p => p.url) || [],
+          'address': {
+            '@type': 'PostalAddress',
+            'streetAddress': () => organization.value!.address.streetAddress,
+            'postalCode': () => organization.value!.address.postalCode,
+            'addressLocality': () => organization.value!.address.addressLocality,
+            'addressRegion': () => organization.value!.address.addressLocality,
+            'addressCountry': 'DZ',
+          },
+        },
       },
     ],
   })
-
-  if (import.meta.server) {
-    useSeoMeta({
-      ogType: 'website',
-      ogImageType: 'image/jpeg',
-      twitterSite: '@Univerweb',
-      twitterCard: 'summary_large_image',
-      twitterImageType: 'image/jpeg',
-      colorScheme: 'light dark',
-    })
-
-    useHead({
-      link: [
-        { rel: 'icon', type: 'image/png', href: '/favicon-48x48.png', sizes: '48x48' },
-        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
-        { rel: 'shortcut icon', href: '/favicon.ico' },
-        { rel: 'apple-touch-icon', href: '/apple-touch-icon.png', sizes: '180x180' },
-        { rel: 'manifest', href: '/site.webmanifest' },
-      ],
-    })
-  }
 }
