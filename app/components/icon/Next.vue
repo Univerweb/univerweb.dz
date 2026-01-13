2e90e593b502dad62590797335d468d1f85d0c88
Houssem Nedjar <h.nedjar@univerweb.dz>
2026-01-13 10:18:06 +0100

    fix: btn svg

--- app/components/icon/Next.vue
+++ app/components/icon/Next.vue
@@ -1,5 +1,5 @@
 <template>
-  <AppIcon :width="8" :height="14">
+  <AppIcon :width="8" :height="14" class="next">
     <path d="m0 2.2 5.067 5.067L0 12.333 1.467 13.8l5.066-5.067L8 7.267 6.533 5.8 1.467.733 0 2.2Z" />
   </AppIcon>
 </template>
